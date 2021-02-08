55991eb3b5677346ba34cc81fe511c149097d9f9
Paolo Graziani <paolo.graziani@studenti.univr.it>
2021-02-08 18:27:27 +0100

    Edit sum_and_difference_mybot.java

--- problems/sum/bots/Java/sum_and_difference_mybot.java
+++ problems/sum/bots/Java/sum_and_difference_mybot.java
@@ -7,9 +7,12 @@ public class sum_and_difference_mybot {
     public static void main(String[] args) {
       Scanner scan = new Scanner(System.in);
       String spoon ="";
-      do {
+      while(true) {
        spoon = scan.nextLine();
-      }while(!"?".equals(spoon.split(" ")[0]));
+
+      while(!"?".equals(spoon.split(" ")[0])){
+          spoon=scan.nextLine();
+      }
       int s = Integer.parseInt(spoon.split(" ")[1]);
       int d = Integer.parseInt(spoon.split(" ")[2]); 
       int x1 = (s + d) / 2;
