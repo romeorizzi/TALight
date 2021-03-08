8baa8f3c366b86c24e2fc257681c75a633079a84
Paolo Graziani <paolo.graziani@studenti.univr.it>
2021-03-08 14:09:59 +0100

    java fix services

--- problems/sum/bots/Java/sum_and_product_mybot.java
+++ problems/sum/bots/Java/sum_and_product_mybot.java
@@ -1,4 +1,5 @@
 import static java.lang.Math.sqrt;
+
 import java.util.Scanner;
 
 public class sum_and_product_mybot {
@@ -8,21 +9,21 @@ public class sum_and_product_mybot {
      */
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String spoon ="";
-        String s,p ="";
-        int x1,x2;
+        String spoon = "";
+        String s, p = "";
+        int x1, x2;
 
-        while(true) {
-            spoon=scan.nextLine();
-            while(!spoon.split(" ")[0].equals("?")) {
-                spoon=scan.nextLine();
+        while (true) {
+            spoon = scan.nextLine();
+            while (!spoon.split(" ")[0].equals("?")) {
+                spoon = scan.nextLine();
             }
-            s=spoon.split(" ") [1];
-            p=spoon.split(" ") [2];
-            int rad =(int)sqrt(Integer.parseInt(s)*Integer.parseInt(s)-4*Integer.parseInt(p));
-            x1=(Integer.parseInt(s)-rad)/2;
-            x2=Integer.parseInt(s) -x1;
-            System.out.println(x1 + " "+x2);
+            s = spoon.split(" ")[1];
+            p = spoon.split(" ")[2];
+            int rad = (int) sqrt(Integer.parseInt(s) * Integer.parseInt(s) - 4 * Integer.parseInt(p));
+            x1 = (Integer.parseInt(s) - rad) / 2;
+            x2 = Integer.parseInt(s) - x1;
+            System.out.println(x1 + " " + x2);
         }
     }
 }
