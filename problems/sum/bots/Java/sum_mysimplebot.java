ed99ad65e4efda1d6452e1c77136d13d5e362393
Paolo Graziani <paolo.graziani@studenti.univr.it>
2021-03-08 14:43:04 +0100

    bot in Java per sum

---
+++ problems/sum/bots/Java/sum_mysimplebot.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class sum_my_simplebot {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String spoon = "";
+        String s, p = "";
+        int x1, x2;
+        int n;
+
+        while (true) {
+            spoon = scan.nextLine();
+            while (!spoon.split(" ")[0].equals("?")) {
+                spoon = scan.nextLine();
+            }
+            n = Integer.parseInt(spoon.split(" ")[1]);
+            System.out.println(n + " 0");
+        }
+    }
+}
