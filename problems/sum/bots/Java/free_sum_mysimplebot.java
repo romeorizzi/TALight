009af5ffbc99dafce9ef808bf1000ec369e82a2c
Romeo Rizzi <romeo.rizzi@univr.it>
2021-03-08 15:43:28 +0100

    revised the java bots for problem sum service sum

--- problems/sum/bots/Java/free_sum_mysimplebot.java
+++ problems/sum/bots/Java/free_sum_mysimplebot.java
@@ -1,22 +1,17 @@
 import java.util.Scanner;
-public class sum_mymaxproductbot {
-
-
+public class free_sum_mysimplebot {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String spoon = "";
-        String s, p = "";
-        int x1, x2;
         int n;
 
         while (true) {
             spoon = scan.nextLine();
-            while (!spoon.split(" ")[0].equals("?")) {
+            while (!(spoon.charAt(0) == '?')) {
                 spoon = scan.nextLine();
             }
             n = Integer.parseInt(spoon.split(" ")[1]);
-
-            System.out.println(n / 2 + " " + (n + 1) / 2);
+            System.out.println(n + " 0");
         }
     }
 }
