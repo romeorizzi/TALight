7d65a9281f1b5d62e0e803f453872119a5886d67
leonardo ballini <leonardo.ballini@protonmail.com>
2021-03-15 18:40:40 +0100

    Updated 3SAT messages

--- example_problems/tutorial/3SAT/services/check_f_representation_server.py
+++ example_problems/tutorial/3SAT/services/check_f_representation_server.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 # METADATA OF THIS TAL_SERVICE:
-problem="3SAT"
-service="check_f_representation"
+problem = "3SAT"
+service = "check_f_representation"
 args_list = [
-    ('f_k',str),
-    ('f_3',str),
-    ('lang',str),
+    ('f_k', str),
+    ('f_3', str),
+    ('lang', str),
+    ('ISATTY', bool),
 ]
 
 from sys import stderr, exit, argv
@@ -15,20 +16,22 @@ from multilanguage import Env, Lang, TALcolors
 import re
 import itertools
 
+
 def valid_cnf_string(string, regex):
     matches = re.match(regex,
-        string)
-    if(matches != None):
+                       string)
+    if (matches != None):
         if (matches.span()[1] == len(string)):
             return True
-        else :
+        else:
             error_position = matches.span()[1]
     else:
         error_position = 0
-    TAc.print("\n\nError found in the string at the position {}".format(error_position),"red")
-    TAc.print(string[error_position:error_position + 50],"yellow")
-    TAc.print("^","red")
+    TAc.print("\n\n# Error found in the string at the position {}".format(error_position), "red")
+    TAc.print("# " + string[error_position:error_position + 50], "yellow")
+    TAc.print("# ^", "red")
     return False
+
 
 def check_equivalence(cnf1, cnf2):
     literals1 = SAT_lib.get_literals(cnf1)
@@ -39,7 +42,7 @@ def check_equivalence(cnf1, cnf2):
     lit1_n = len(literals1)
     new_lit_n = len(new_literals)
 
-    ass=set()
+    ass = set()
     for out_seq in itertools.product([True, False], repeat=lit1_n):
         out_ass = list(zip(literals1, out_seq))
         if new_lit_n > 0:
@@ -52,55 +55,67 @@ def check_equivalence(cnf1, cnf2):
                     found = True
                     break
             if a and not found:
-                readable_ass = '\n'.join(sorted(set(map(lambda x: "\t" + x[0] + " = " + str(x[1]), out_ass))))
-                TAc.print("\n\nThe two cnf are not equivalent", "red")
-                TAc.print("For the assignment: \n{} \nthere is not an assignment for the variables set: {} such that"
-                      " the cnf1:\n\t{}\nand the cnf2:\n\t{} \ngives as output the same result.\n"
-                      "In fact the first one returns True while the second one returns always False no matter what value "
-                      "we assign to the variables in the set: {}".
-                      format(readable_ass, new_literals, SAT_lib.to_string(cnf1).replace("and ", "and\n\t"),
-                             SAT_lib.to_string(cnf2).replace("and ", "and\n\t"), new_literals), "yellow")
+                readable_ass = '\n#'.join(sorted(set(map(lambda x: "\t" + x[0] + " = " + str(x[1]), out_ass))))
+                TAc.print("\n\n# The two cnf are not equivalent\n# For the assignment:", "red")
+                TAc.print("# {}"
+                          "\n# there is not an assignment for the variables set:"
+                          "\n#\t{}"
+                          "\n# such that the cnf1:"
+                          "\n#\t{})"
+                          "\n# and the cnf2:"
+                          "\n#\t{}"
+                          "\n# gives as output the same result."
+                          .format(readable_ass, new_literals, SAT_lib.to_string(cnf1).replace("and ", "and\n#\t"),
+                                  SAT_lib.to_string(cnf2).replace("and ", "and\n#\t")), "yellow"
+                          )
+                TAc.print("# In fact the first one returns True while the second one returns always False "
+                          "\n# no matter what value we assign to the variables in the set: {}".format(new_literals),
+                          "red")
                 exit(1)
             if (found and not a):
-                readable_ass = '\n'.join(sorted(set(map(lambda x: "\t" + x[0] + " = " + str(x[1]), ass))))
-                TAc.print("\n\nThe two cnf are not equivalent", "red")
-                TAc.print("For the assignment: \n{} \n the cnf1:\n\t{}\nand the cnf2:\n\t{} \n are not equivalent.\n"
-                          "In fact the second ones returns True while the first one returns False ".format(
-                    readable_ass, SAT_lib.to_string(cnf1).replace("and ", "and\n\t"),
-                                 SAT_lib.to_string(cnf2).replace("and ", "and\n\t")), "yellow")
+                readable_ass = '\n#'.join(sorted(set(map(lambda x: "\t" + x[0] + " = " + str(x[1]), ass))))
+                TAc.print("\n\n#The two cnf are not equivalent", "red")
+                TAc.print(
+                    "# For the assignment: \n#{} \n# the cnf1:\n#\t{}\n# and the cnf2:\n#\t{} \n# are not equivalent.".
+                        format(readable_ass, SAT_lib.to_string(cnf1).replace("and ", "and\n#\t"),
+                               SAT_lib.to_string(cnf2).replace("and ", "and\n#\t")), "yellow")
+                TAc.print("# In fact the first one returns False while the second ones returns True","red")
                 exit(1)
         else:
             a = SAT_lib.check_sol(cnf1, out_ass)
             b = SAT_lib.check_sol(cnf2, out_ass)
             if (a != b):
-                readable_ass = '\n'.join(sorted(set(map(lambda x: "\t" + x[0] + " = " + str(x[1]), out_ass))))
-                TAc.print("\n\nThe two cnf are not equivalent","red")
-                TAc.print("For the assignment: \n{} \nt"
-                      " the cnf1:\n\t{}\nand the cnf2:\n\t{} \ngives as output a different result.\n"
-                      "In fact the first one returns {} while the second one returns {}.".
-                      format(readable_ass, SAT_lib.to_string(cnf1).replace("and ", "and\n\t"),
-                             SAT_lib.to_string(cnf2).replace("and ", "and\n\t"), a, b), "yellow")
+                readable_ass = '\n#'.join(sorted(set(map(lambda x: "\t" + x[0] + " = " + str(x[1]), out_ass))))
+                TAc.print("\n\n# The two cnf are not equivalent", "red")
+                TAc.print("# For the assignment: \n#{} \n"
+                          "# the cnf1:\n#\t{}\n# and the cnf2:\n#\t{} \n# gives as output a different result.".
+                          format(readable_ass, SAT_lib.to_string(cnf1).replace("and ", "and\n#\t"),
+                                 SAT_lib.to_string(cnf2).replace("and ", "and\n#\t")), "yellow")
+                TAc.print("# In fact the first one returns {} while the second one returns {}.".format(a, b), "red")
                 exit(1)
-    TAc.print("\nCongraturation!!!\nThe two cnf are equivalent", "green")
+    TAc.print("\n# Congraturation!!!\n# The two cnf are equivalent", "green")
 
-ENV =Env(problem, service, args_list)
-TAc =TALcolors(ENV)
-LANG=Lang(ENV, TAc, lambda fstring: eval(f"f'{fstring}'"))
+
+ENV = Env(problem, service, args_list)
+TAc = TALcolors(ENV)
+LANG = Lang(ENV, TAc, lambda fstring: eval(f"f'{fstring}'"))
 if not ENV['silent']:
     TAc.print(LANG.opening_msg, "green")
 
 if (ENV['f_k'] == 'lazy_input'):
-    TAc.print("\nInsert first CNF:", "green")
+    TAc.print("\n# Insert the K-SAT formula:", "green")
     clause = input()
-    if (not valid_cnf_string(clause,'^(\s*\(\s*(\s*!?\s*x[1-9])(\s*or\s*!?\s*x[1-9])*\s*\)(\s*and\s*\(\s*(\s*!?\s*x[1-9])(\s*or\s*!?\s*x[1-9])*\s*\))*\s*)')):
+    if (not valid_cnf_string(clause,
+                             '^(\s*\(\s*(\s*!?\s*x[1-9])(\s*or\s*!?\s*x[1-9])*\s*\)(\s*and\s*\(\s*(\s*!?\s*x[1-9])(\s*or\s*!?\s*x[1-9])*\s*\))*\s*)')):
         exit(1)
 else:
     clause = ENV['f_k']
 
 if (ENV['f_3'] == 'lazy_input'):
-    TAc.print("\nInsert second CNF:", "green")
+    TAc.print("\n# Insert the 3-SAT formula:", "green")
     representing_formula = input()
-    if (not valid_cnf_string(representing_formula,'^(\s*\(\s*(\s*!?\s*(x|y)[1-9])(\s*or\s*!?\s*(x|y)[1-9])*\s*\)(\s*and\s*\(\s*(\s*!?\s*(x|y)[1-9])(\s*or\s*!?\s*(x|y)[1-9])*\s*\))*\s*)')):
+    if (not valid_cnf_string(representing_formula,
+                             '^(\s*\(\s*(\s*!?\s*(x|y)[1-9])(\s*or\s*!?\s*(x|y)[1-9])*\s*\)(\s*and\s*\(\s*(\s*!?\s*(x|y)[1-9])(\s*or\s*!?\s*(x|y)[1-9])*\s*\))*\s*)')):
         exit(1)
 else:
     representing_formula = ENV['f_3']
@@ -109,8 +124,8 @@ cnf1 = SAT_lib.to_cnf(clause)
 cnf2 = SAT_lib.to_cnf(representing_formula)
 for disj in cnf2:
     if len(disj) > 3:
-        TAc.print("\n\nThe second cnf inserted is not a valid 3-SAT.\n"
-                  "In fact {} contains more than 3 literals".format(SAT_lib.to_string([disj])), "red")
+        TAc.print("\n\n# The second cnf inserted is not a valid 3-SAT.\n"
+                  "# In fact {} contains more than 3 literals".format(SAT_lib.to_string([disj])), "red")
         exit(1)
 
 check_equivalence(cnf1, cnf2)
--- example_problems/tutorial/3SAT/services/check_one_clause_representation_server.py
+++ example_problems/tutorial/3SAT/services/check_one_clause_representation_server.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 # METADATA OF THIS TAL_SERVICE:
-problem="3SAT"
-service="check_one_clause_representation"
+problem = "3SAT"
+service = "check_one_clause_representation"
 args_list = [
-    ('clause',str),
-    ('representing_formula',str),
-    ('lang',str),
+    ('clause', str),
+    ('representing_formula', str),
+    ('lang', str),
 ]
 
 from sys import stderr, exit, argv
@@ -15,20 +15,22 @@ from multilanguage import Env, Lang, TALcolors
 import re
 import itertools
 
+
 def valid_cnf_string(string, regex):
     matches = re.match(regex,
-        string)
-    if(matches != None):
-        if (matches.span()[1] == len(string)):
+                       string)
+    if matches is not None:
+        if matches.span()[1] == len(string):
             return True
-        else :
+        else:
             error_position = matches.span()[1]
     else:
         error_position = 0
-    TAc.print("# Error found in the string at the position {}".format(error_position),"red")
-    TAc.print("# "+string[error_position:error_position + 50],"yellow")
-    TAc.print("# ^","red")
+    TAc.print("# Error found in the string at the position {}".format(error_position), "red")
+    TAc.print("# " + string[error_position:error_position + 50], "yellow")
+    TAc.print("# ^", "red")
     return False
+
 
 def check_equivalence(cnf1, cnf2):
     literals1 = SAT_lib.get_literals(cnf1)
@@ -39,7 +41,7 @@ def check_equivalence(cnf1, cnf2):
     lit1_n = len(literals1)
     new_lit_n = len(new_literals)
 
-    ass=set()
+    ass = set()
     for out_seq in itertools.product([True, False], repeat=lit1_n):
         out_ass = list(zip(literals1, out_seq))
         if new_lit_n > 0:
@@ -52,60 +54,62 @@ def check_equivalence(cnf1, cnf2):
                     found = True
                     break
             if a and not found:
-                readable_ass = '\n'.join(sorted(set(map(lambda x: "\t" + x[0] + " = " + str(x[1]), out_ass))))
+                readable_ass = '\n#'.join(sorted(set(map(lambda x: "\t" + x[0] + " = " + str(x[1]), out_ass))))
                 TAc.print("# The two cnf are not equivalent", "red")
-                TAc.print("# For the assignment: \n# {} \n# there is not an assignment for the variables set: {} such that"
-                      "# the clause:\n\t# {}\n#  and the representing formula:\n\t#  {} \n# gives as output the same result.\n"
-                      "# In fact the first one returns True while the second one returns always False no matter what value "
-                      "we assign to the variables in the set: {}".
-                      format(readable_ass, new_literals, SAT_lib.to_string(cnf1).replace("and ", "and\n\t"),
-                             SAT_lib.to_string(cnf2).replace("and ", "and\n\t"), new_literals), "yellow")
+                TAc.print(
+                    "# For the assignment: \n# {} \n# there is not an assignment for the variables set: {} such that"
+                    "# the clause:\n#\t {}\n# and the representing formula:\n#\t{} \n# gives as output the same result.\n"
+                    "# In fact the first one returns True while the second one returns always False no matter what value "
+                    "we assign to the variables in the set: {}".
+                    format(readable_ass, new_literals, SAT_lib.to_string(cnf1).replace("and ", "and\n#\t"),
+                           SAT_lib.to_string(cnf2).replace("and ", "and\n#\t"), new_literals), "yellow")
                 exit(1)
             if (found and not a):
                 readable_ass = '\n# '.join(sorted(set(map(lambda x: "\t" + x[0] + " = " + str(x[1]), ass))))
                 TAc.print("#  The two cnf are not equivalent", "red")
-                TAc.print("#  For the assignment: \n# {} \n# the clause:\n#\t{}\n# and the representing formula:\n#\t{} \n# are not equivalent.\n"
-                          "# In fact the second ones returns True while the first one returns False ".format(
-                    readable_ass, SAT_lib.to_string(cnf1).replace("and ", "and\n#\t"),
-                                 SAT_lib.to_string(cnf2).replace("and ", "and\n#\t")), "yellow")
+                TAc.print(
+                    "#  For the assignment: \n# {} \n# the clause:\n#\t{}\n# and the representing formula:\n#\t{} \n# are not equivalent.\n"
+                    "# In fact the second ones returns True while the first one returns False ".format(
+                        readable_ass, SAT_lib.to_string(cnf1).replace("and ", "and\n#\t"),
+                        SAT_lib.to_string(cnf2).replace("and ", "and\n#\t")), "yellow")
                 exit(1)
         else:
             a = SAT_lib.check_sol(cnf1, out_ass)
             b = SAT_lib.check_sol(cnf2, out_ass)
             if (a != b):
                 readable_ass = '\n# '.join(sorted(set(map(lambda x: "\t" + x[0] + " = " + str(x[1]), out_ass))))
-                TAc.print("# The two cnf are not equivalent","red")
-                TAc.print("# For the assignment: \n{} \n"
-                      "# the clause:\n\t{}\n# and the representing formula:\n\t{} \n# gives as output a different result.\n"
-                      "# In fact the first one returns {} while the second one returns {}.".
-                      format(readable_ass, SAT_lib.to_string(cnf1).replace("and ", "and\n\t"),
-                             SAT_lib.to_string(cnf2).replace("and ", "and\n#\t"), a, b), "yellow")
+                TAc.print("# The two cnf are not equivalent", "red")
+                TAc.print("# For the assignment: \n#{} \n"
+                          "# the clause:\n#\t{}\n# and the representing formula:\n#\t{} \n# gives as output a different result.\n"
+                          "# In fact the first one returns {} while the second one returns {}.".
+                          format(readable_ass, SAT_lib.to_string(cnf1).replace("and ", "and\n\t"),
+                                 SAT_lib.to_string(cnf2).replace("and ", "and\n#\t"), a, b), "yellow")
                 exit(1)
     TAc.print("\n# Congraturation!!!\nThe two cnf are equivalent", "green")
 
-ENV =Env(problem, service, args_list)
-TAc =TALcolors(ENV)
-LANG=Lang(ENV, TAc, lambda fstring: eval(f"f'{fstring}'"))
+
+ENV = Env(problem, service, args_list)
+TAc = TALcolors(ENV)
+LANG = Lang(ENV, TAc, lambda fstring: eval(f"f'{fstring}'"))
 if not ENV['silent']:
     TAc.print(LANG.opening_msg, "green")
 
 if (ENV['clause'] == 'lazy_input'):
-    TAc.print("\nInsert clause:", "green")
+    TAc.print("\n# Insert clause:", "green")
     clause = input()
-    if (not valid_cnf_string(clause,'^((\s*!?\s*(x|y)[1-9])(\s*or\s*!?\s*(x|y)[1-9])*\s*|lazy_input)')):
+    if (not valid_cnf_string(clause, '^((\s*!?\s*(x|y)[1-9])(\s*or\s*!?\s*(x|y)[1-9])*\s*|lazy_input)')):
         exit(1)
 else:
     clause = ENV['clause']
 
 if (ENV['representing_formula'] == 'lazy_input'):
-    TAc.print("\nInsert the representing formula:", "green")
+    TAc.print("\n# Insert the representing formula:", "green")
     representing_formula = input()
-    if (not valid_cnf_string(representing_formula,'^(\s*\(\s*(\s*!?\s*(x|y)[1-9])(\s*or\s*!?\s*(x|y)[1-9])*\s*\)(\s*and\s*\(\s*(\s*!?\s*(x|y)[1-9])(\s*or\s*!?\s*(x|y)[1-9])*\s*\))*\s*|lazy_input)')):
+    if (not valid_cnf_string(representing_formula,
+                             '^(\s*\(\s*(\s*!?\s*(x|y)[1-9])(\s*or\s*!?\s*(x|y)[1-9])*\s*\)(\s*and\s*\(\s*(\s*!?\s*(x|y)[1-9])(\s*or\s*!?\s*(x|y)[1-9])*\s*\))*\s*|lazy_input)')):
         exit(1)
 else:
     representing_formula = ENV['representing_formula']
-
-
 
 cnf1 = SAT_lib.to_cnf(clause)
 cnf2 = SAT_lib.to_cnf(representing_formula)
