fee4beb4f603640adb7312e2f1670784873db964
Paolo Graziani <paolo.graziani@studenti.univr.it>
2021-03-15 18:47:27 +0100

    Fix error TALcolor for java

--- TAL_lib/Java/TALcolors.java
+++ TAL_lib/Java/TALcolors.java
@@ -10,52 +10,87 @@
  * @author UTENTE
  */
 import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
 
 public class TALcolors {
-    public int num_NO=0;
-    public int num_OK=0;
-    private String msg_style ="";
-    private String colors="";
-    colorOption co=new colorOption();
-    boolean colored_print = Boolean.parseBoolean(System.getenv("TAL_ISATTY"));
+    public int num_NO = 0;
+    public int num_OK = 0;
+    private String msg_style = "";
+    private String colors = "";
 
-    public void print(String msg_text,String color,Object characterFormats,String end){
-        if(characterFormats!=null)
-            this.msg_style=characterFormats.toString();
-        if(color !=null) {
-            if(color.equals("red")) {
-                System.out.println(co.color16(colorOption.Color16.FG_RED,msg_text));
+    String colored_print = System.getenv("ISATTY");
+
+    public void print(String msg_text, String color, List<String> characterFormats, String end) {
+        colorOption co = new colorOption();
+        if (characterFormats != null)
+            for(String character:characterFormats) {
+                if(character.equals("bold"))
+                    co.bold();
+                if(character.equals("blink"))
+                    co.blink();
             }
-            if(color.equals("green")){
-                System.out.println(co.color16(colorOption.Color16.FG_GREEN,msg_text));
+        if (color != null) {
+            if (color.equals("red")) {
+                System.out.println(co.color16(colorOption.Color16.FG_RED, msg_text));
+                return;
             }
-            if(color.equals("white")) {
-                System.out.println(co.color16(colorOption.Color16.FG_WHITE,msg_text));
+            if (color.equals("green")) {
+                System.out.println(co.color16(colorOption.Color16.FG_GREEN, msg_text));
+                return;
             }
+            if (color.equals("white")) {
+                System.out.println(co.color16(colorOption.Color16.FG_WHITE, msg_text));
+                return;
+            }
+            if (color.equals("yellow")) {
+                System.out.println(co.color16(colorOption.Color16.FG_YELLOW, msg_text));
+                return;
+            }
+            else {
+                print("Warning!This color: "+color+" is not supported","yellow",null,null);
+                System.out.println(msg_text);
+            }
+
 
         }
 
 
     }
-    public void NO(){
-        num_NO+=1;
-        this.print("No!","red",null,"");
+
+    public void NO() {
+        num_NO += 1;
+        List<String> car = new ArrayList<> ();
+        car.add("bold");
+        car.add("blink");
+        this.print("No!", "red",car , "");
     }
-    public void OK(){
-        num_OK+=1;
-        this.print("OK", "green", null,"");
+
+    public void OK() {
+        num_OK += 1;
+        List<String> car = new ArrayList<>();
+        car.add("bold");
+        car.add("blink");
+        this.print("OK", "green", car, "");
     }
-    public void GoBored(){
-        this.print("!(I got bored", "white",null, null);
+
+    public void GoBored() {
+        this.print("!(I got bored", "white", null, null);
     }
+
     public void Finished() {
-        this.print("!(We have finished)Correct answers:"+this.num_OK+"/"+(this.num_OK+this.num_NO),"white",null,null);
+        this.print("!(We have finished)Correct answers:" + this.num_OK + "/" + (this.num_OK + this.num_NO), "violet", null, null);
     }
 
 
+    public static void main(String args[]) {
 
+        TALcolors tac = new TALcolors();
+        System.out.println(tac.colored_print);
+        tac.NO();
+        tac.Finished();
+    }
 }
-
 
 /*
 from sys import stdout, stderr, exit, argv
