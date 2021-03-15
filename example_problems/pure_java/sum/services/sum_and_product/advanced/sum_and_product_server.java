5e7b6b8acd30b14ca77b8dc6ee2b5881027db0b1
Paolo Graziani <paolo.graziani@studenti.univr.it>
2021-03-15 22:14:54 +0100

    Reorganised Directory sum_and_product for java. First implementation for sum_and_product advance.The next task is to implement Lang!

---
+++ example_problems/pure_java/sum/services/sum_and_product/advanced/sum_and_product_server.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
+
+public class sum_and_product_server {
+    public static void main(String[] args) {
+        String problem = "sum";
+        String service= "sum_and_product";
+        Map<String,String>args_list=new HashMap<>();
+        args_list.put("NUM_QUESTIONS","int");
+        args_list.put("NUMBERS","String");
+        args_list.put("LANG","String");
+        args_list.put("ISATTY","String");
+
+        Env env= new Env(problem,service,args_list);
+        TALcolors TAc = new TALcolors();
+
+        Scanner scan = new Scanner(System.in);
+        String spoon = "";
+        String numbers = "";
+        int num_question=0;
+        int x = 0;
+        int y = 0;
+        Random randrange = new Random();
+        boolean gen_new_pair = true;
+
+        for(int i=0;i <(int)env.ENV("NUM_QUESTIONS");i++) {
+            if(env.ENV("NUMBERS").equals("onedigit")) {
+                x= randrange.nextInt(10);
+                y=randrange.nextInt(10);
+            }
+            else if(env.ENV("NUMBERS").equals("twodigits")) {
+                x=randrange.nextInt(100);
+                y=randrange.nextInt(100);
+            }
+            else {
+                x=randrange.nextInt(2*32);
+                y=randrange.nextInt(2*32);
+            }
+            int sum = x+y;
+            int prod=x*y;
+            List<String> carFormat = new ArrayList<>();
+            carFormat.add("bold");
+            TAc.print("? "+sum +" "+ prod,"yellow",carFormat,null);
+            spoon = scan.nextLine();
+            while(spoon.charAt(0)=='#') {
+                spoon=scan.nextLine();
+            }
+            int a = Integer.parseInt(spoon.split(" ")[0]);
+            int b = Integer.parseInt(spoon.split(" ")[1]);
+            gen_new_pair = false;
+            if (a+b > x+y)
+                System.out.println("No! indeed,"+a+ "+"+b+"="+(a+b)+">"+(x+y)+".");
+            else if (a+b < x+y)
+                System.out.println("No! indeed,"+a+ "+"+b+"="+(a+b)+"<"+(x+y)+".");
+            else if (a*b > x*y)
+                System.out.println("No! indeed,"+a+ "*"+b+"="+(a*b)+">"+(x*y)+".");
+            else if (a*b < x*y)
+                System.out.println("No! indeed,"+a+ "*"+b+"="+(a*b)+">"+(x*y)+".");
+            else {
+                System.out.println("Ok! indeed,"+a+ "+"+b+"="+(a+b)+" and"+a+"*"+b+"="+(a*b)+".");
+                gen_new_pair = false;
+            }
+
+        }
+        System.exit(0);
+    }
+}
