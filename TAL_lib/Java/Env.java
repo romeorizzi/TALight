2c223195d74896dc022cc56a0ff314b0d34c9053
Paolo Graziani <paolo.graziani@studenti.univr.it>
2021-03-15 20:21:41 +0100

    Fix error in Env for java

--- TAL_lib/Java/Env.java
+++ TAL_lib/Java/Env.java
@@ -13,76 +13,55 @@ import java.util.Map;
  *
  * @author UTENTE
  */
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author UTENTE
+ */
 public class Env {
-    private String service_server_fullname="";
-    private String exe_path="";
-    private Map<String,String> environment_variables=System.getenv();
+    private Map<String,Object> item = new HashMap();
+    private Map<String,String> environment_variables=new HashMap();
     private String meta_data=System.getenv("TAL_META_DIR");
     private String problem="";
     private String service="";
     private Object [] arg = {};
 
-    public Env(String exe_path,String problem,String service,Object []argv){
-        this.service_server_fullname=argv[0].toString();
-        this.exe_path=exe_path.split(service_server_fullname)[0];
+    public Env(String problem,String service,Map<String,String>args){
         this.problem=problem;
         this.service=service;
-        Object [] arg = {};
-        for(int i =0;i<argv.length;i++) {
-            if(argv[i] instanceof String)
-                arg[i]=System.getenv("TAL_"+i);
-            else if(argv[i] instanceof Boolean) {
-                argv[i]="1";
-                environment_variables.put("TAL_"+i,"1");
-            }
-            else if(argv[i] instanceof Integer) {
-                for (Map.Entry <String, String> entry: environment_variables.entrySet()) {
-                     if(entry.getKey().equals("TAL_"+i))
-                             arg[i]=Integer.parseInt(entry.getValue());
-                }
-            }
-            else if(argv[i] instanceof Float) {
-                for (Map.Entry <String, String> entry: environment_variables.entrySet()) {
-                     if(entry.getKey().equals("TAL_"+i))
-                             arg[i]=Float.parseFloat(entry.getValue());
-                }
-            }
-            else {
-               System.out.println("# Unrecoverable Error: TypeVariable not yet supported in args list. Used to interpret arg {name}.");
-               System.err.println("# Unrecoverable Error: TypeVariable not yet supported in args list. Used to interpret arg {name}.");
-               System.exit(0);
+        this.environment_variables=args;
+        this.item=ENV(environment_variables);
+    }
+
+
+    private Map<String,Object> ENV(Map<String,String>args) {
+
+        for (Map.Entry <String, String> entry: args.entrySet()) {
+            switch(entry.getValue()) {
+                case "int":
+                    item.put(entry.getKey(),Integer.parseInt(System.getenv("TAL_"+entry.getKey())));
+                    break;
+                case "String":
+                    item.put(entry.getKey(),System.getenv("TAL_"+entry.getKey()));
+                    break;
+                case "Float":
+                    item.put(entry.getKey(),Float.parseFloat(System.getenv("TAL_"+entry.getKey())));
+                    break;
+                default:
+                    System.out.println("# Unrecoverable Error: type "+entry.getValue()+"not yet supported in args list. Used to interpret arg "+entry.getKey()+".");
             }
         }
+        return item;
     }
-    public Object[] getitem() {
-        return arg;
+    public Object ENV(String var) {
+        for (Map.Entry <String, Object> entry: item.entrySet()) {
+            if(entry.getKey().equals(var))
+                return entry.getValue();
+        }
+        return null;
     }
 }
-
-/*
-class Env:
-    
-    def __init__(self, problem, service, args_list):
-        self.service_server_fullname = argv[0]
-        self.exe_path = split(argv[0])[0]
-        self.meta_dir = environ["TAL_META_DIR"]
-        self.problem = problem
-        self.service = service
-        self.args_list = args_list
-        self.arg = {}
-        for name, val_type in args_list:
-            if val_type == str:
-                self.arg[name] = environ[f"TAL_{name}"]
-            elif val_type == bool:
-                self.arg[name] = (environ[f"TAL_{name}"] == "1")
-            elif val_type == int:
-                self.arg[name] = int(environ[f"TAL_{name}"])
-            elif val_type == float:
-                self.arg[name] = float(environ[f"TAL_{name}"])
-            else:
-                for out in [stdout, stderr]:
-                    print(f"# Unrecoverable Error: type {val_type} not yet supported in args list. Used to interpret arg {name}.", file=out)
-                exit(1)
-    def __getitem__(self, key):
-        return self.arg.get(key)
-*/
