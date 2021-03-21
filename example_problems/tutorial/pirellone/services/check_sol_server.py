7bfad0be9d52a16201fbb92faad34cbd1625618b
Aurora <aurora98rossi@gmail.com>
2021-03-21 15:37:05 +0100

    check_sol definitivo

--- example_problems/tutorial/pirellone/services/check_sol_server.py
+++ example_problems/tutorial/pirellone/services/check_sol_server.py
@@ -11,7 +11,7 @@ args_list = [
     ('ISATTY',bool),
 ]
 
-from sys import stderr, exit, argv
+from sys import exit
 import pirellone_lib as pl
 from TALinputs import TALinput
 from multilanguage import Env, Lang, TALcolors
@@ -28,10 +28,10 @@ n=int(input())
 
 if ENV['instance']=='random':
     pirellone, seed=pl.random_pirellone(m, n, ENV['seed'], solvable=True)
-    TAc.print(f"Instance (of seed {seed}): ", "yellow", ["bold"])
+    TAc.print(LANG.render_feedback("instance-seed",f"Instance (of seed {seed}): "), "yellow", ["bold"])
     pl.print_pirellone(pirellone)
 elif ENV['instance']=='mine':
-    TAc.print("Instance to check:", "yellow", ["bold"])
+    TAc.print(LANG.render_feedback("instance-","Instance to check:"), "yellow", ["bold"])
     pirellone=[]
 
     for i in range(m):
@@ -39,15 +39,15 @@ elif ENV['instance']=='mine':
         pirellone.append(row)   
 
 if ENV['coding']=='seq':
-    TAc.print("Sequence of rows and colums: ", "yellow", ["bold"])
+    TAc.print(LANG.render_feedback("sequence-r-c","Sequence of rows and columms: "), "yellow", ["bold"])
     solu=input()
     solu=solu.split()
     pl.off_lista(pirellone,solu,TAc, LANG)
 elif ENV['coding']=='subset':
-    TAc.print("Rows solution: ", "yellow", ["bold"])
-    rs = list(map(int,input().strip().split()))[:m] 
-    TAc.print("Columns solution: ", "yellow", ["bold"])
-    cs = list(map(int,input().strip().split()))[:n]
+    TAc.print(LANG.render_feedback("rows","Rows solution: "), "yellow", ["bold"])
+    rs = TALinput(int, num_tokens=m)
+    TAc.print(LANG.render_feedback("col","Columns solution: "), "yellow", ["bold"])
+    cs = TALinput(int, num_tokens=n)
     pl.off(pirellone,rs,cs,TAc, LANG)
 
     
