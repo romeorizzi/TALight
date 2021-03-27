be9d5108f8be65d5c27956e863494cad118b1715
micheleboldo <michele.boldo@studenti.univr.it>
2021-03-27 18:09:29 +0100

    updated inc_subseq_lib

--- example_problems/tutorial/increasing_subseq/services/eval_subseq_server.py
+++ example_problems/tutorial/increasing_subseq/services/eval_subseq_server.py
@@ -6,13 +6,14 @@ import random
 from TALinputs import TALinput
 from multilanguage import Env, Lang, TALcolors
 
-from increasing_subsequence_lib import list_to_string, parse_input, generate_random_seq, get_rand_subseq, get_not_subseq, is_subseq_with_position, remove_duplicate_spaces
+from increasing_subsequence_lib import list_to_string, parse_input, get_yes_certificate ,generate_random_seq, get_rand_subseq, get_not_subseq, is_subseq_with_position, remove_duplicate_spaces
 
 # METADATA OF THIS TAL_SERVICE:
 problem="increasing_subseq"
-service="is_subseq_server"
+service="eval_subseq_server"
 args_list = [
     ('times',str),
+    ('cert', bool),
     ('lang',str),
 ]
 ENV =Env(problem, service, args_list)
@@ -28,6 +29,7 @@ length = int(ENV["times"])
 TAc.print("\nIn this problem you are given a sequence of numbers T. You are asked to evaluate whether a sub-sequence s is part of T "+str(length)+" times. \nAnswer \"y\" if you think s is a subsequence of T, \"n\" otherwise.", "green")
 string_T = generate_random_seq(10, 100)
 TAc.print(list_to_string(string_T), "green")
+cert = ENV['cert']
 
 for i in range(0, length):
     x = random.randint(1,10)
@@ -47,9 +49,23 @@ for i in range(0, length):
         exit(0)
     ret = is_subseq_with_position(string_s,string_T)
     if ret[0] == res:
-        TAc.print("OK, your answer is correct!\n\n","green")
+        TAc.print("OK, your answer is correct!\n","green")
     else:
-        TAc.print("NO, your answer isn't correct.\n\n","red")
+        TAc.print("NO, your answer isn't correct.\n","red")
+        
+        if cert:
+            if ret[0]:
+                print(remove_duplicate_spaces(list_to_string(string_T)))
+                print(get_yes_certificate(string_T, ret[1]))
+            else:
+                if not ret[1]:
+                    print("T doesn't contains s")
+                else:
+                    print("T contains only these elements of s")
+                    print(remove_duplicate_spaces(list_to_string(string_T)))
+                    print(get_yes_certificate(string_T, ret[1]))
+        print("\n")           
+                    
 
 
 
