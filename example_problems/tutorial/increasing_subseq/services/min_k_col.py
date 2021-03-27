96ef0def1d8bea7ad06d8ad2b5eb64ba0a5d75f7
micheleboldo <michele.boldo@studenti.univr.it>
2021-03-27 16:46:23 +0100

    updated inc_subseq_lib

--- example_problems/tutorial/increasing_subseq/services/min_k_col.py
+++ example_problems/tutorial/increasing_subseq/services/min_k_col.py
@@ -21,7 +21,7 @@ LANG=Lang(ENV, TAc, lambda fstring: eval(f"f'{fstring}'"))
 
 #if not ENV['silent']:
 #    TAc.print(LANG.opening_msg, "green")
-TAc.print("\nIn this problem you are asked to enter a sequence T of numbers with a blank space between each number (example: 12 34 56 ...) and a sequence s. You will be told if s is a sub-sequence of T.", "green")
+TAc.print("\nYou will be given a sequence of numbers, and you have to enter a minimum coloring, where all elements of the same color represent a non-increasing sequence.", "green")
 T = generate_random_seq(10,100)
 string_T = list_to_string(T)
 TAc.print("T: "+string_T+"\n\n","green")
@@ -32,13 +32,13 @@ mdc = min_decreasing_col(T)
 n_col = n_coloring(mdc)
 
 
-TAc.print("Insert your coloring:\n","green")
+TAc.print("Insert your coloring (example: 1 3 1 ... where 1 = color_1):\n","green")
 k = input()
 ok = bool(re.match(r"^((([1-9][0-9]{0,9} *){9}[1-9][0-9]{0,9}))$", k))
 if ok:
     color = parse_input(k)
 else:
-    TAc.print("\n\nWRONG INPUT FORMAT: this is not a sequence of numbers (example: 12 34 56 ...).\n", "red")
+    TAc.print("\n\nWRONG INPUT FORMAT: this is not a sequence of color (example: 1 3 1 ... where 1 = color_1).\n", "red")
     exit(0)
 
 
