3b27f744a062af19fafd2c3e039775c41bad482b
Romeo Rizzi <romeo.rizzi@univr.it>
2021-04-04 07:39:17 +0200

    UTF-8 in sum_and_difference_mybot.py

--- example_problems/tutorial/sum/bots/python/sum_and_product_mybot.py
+++ example_problems/tutorial/sum/bots/python/sum_and_product_mybot.py
@@ -6,6 +6,7 @@ while True:
     while spoon[0] != '?':
         spoon = input().strip()
     s,p = map(int, spoon[1:].split() )
-    x1 = int((s - math.sqrt(s*s-4*p))//2)
+    Δ = int(math.sqrt(s*s-4*p))
+    x1 = (s - Δ)//2
     x2 = s - x1
     print(f"{x1} {x2}")
