e639b919078a935bb792ac741d84d11b98ed497e
Matteo Zavatteri <matteo.zavatteri@gmail.com>
2021-04-25 08:21:35 +0200

    warm up

--- example_problems/tutorial/regex/services/TAL_recognizes_server.py
+++ example_problems/tutorial/regex/services/TAL_recognizes_server.py
@@ -7,22 +7,15 @@ ENV={}
 ENV['r'] = environ["TAL_r"]
 ENV['s'] = environ["TAL_s"]
 
-feedback = {"regex" : False, "memebership" : False}
-
 try:
     e = re.compile(ENV['r'])
-    feedback["regex"] = True
 except:
-    pass
-
-if not feedback["regex"]:
     print("Error! {} is not a regular expression.".format(ENV['r']))
     exit(0)    
+
 print("Good! {} is a regular expression.".format(ENV['r']))
 
-feedback["memebership"] = (re.fullmatch(ENV['r'], ENV['s']) is not None)
-
-if not feedback["memebership"]:
+if re.fullmatch(ENV['r'], ENV['s']) is None:
     print("Error! {} does not belong to {}.".format(ENV['s'], ENV['r']))
     exit(0)
 
