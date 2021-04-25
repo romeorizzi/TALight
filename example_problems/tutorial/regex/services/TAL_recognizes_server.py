b533d07d6ca2c43b73c3323906101b8562d3abe3
Matteo Zavatteri <matteo.zavatteri@gmail.com>
2021-04-25 08:20:09 +0200

    warm up

--- example_problems/tutorial/regex/services/TAL_recognizes_server.py
+++ example_problems/tutorial/regex/services/TAL_recognizes_server.py
@@ -3,16 +3,11 @@ from sys import stderr, exit
 import re
 from os import environ
 
-
 ENV={}
 ENV['r'] = environ["TAL_r"]
 ENV['s'] = environ["TAL_s"]
 
-#print("Hi there, please enter a regular expression and a string.")
-#print("I'll tell you if the string belongs to the regular expression.")
-#pattern, string = input().split()
-
-feedback = {"regex" : False, "string_memebership" : False}
+feedback = {"regex" : False, "memebership" : False}
 
 try:
     e = re.compile(ENV['r'])
@@ -25,9 +20,9 @@ if not feedback["regex"]:
     exit(0)    
 print("Good! {} is a regular expression.".format(ENV['r']))
 
-feedback["string_memebership"] = (re.fullmatch(ENV['r'], ENV['s']) is not None)
+feedback["memebership"] = (re.fullmatch(ENV['r'], ENV['s']) is not None)
 
-if not feedback["string_memebership"]:
+if not feedback["memebership"]:
     print("Error! {} does not belong to {}.".format(ENV['s'], ENV['r']))
     exit(0)
 
