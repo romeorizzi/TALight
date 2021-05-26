1cc974d46a42ea3c77b9356274d538326a3acc7d
SofiaCaleffi <78377980+SofiaCaleffi@users.noreply.github.com>
2021-05-26 11:32:41 +0200

    Add files via upload

---
+++ example_problems/tutorial/strongly_connected_components/services/eval_find_scc_server.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+from sys import stderr, exit, argv
+
+import collections
+
+from multilanguage import Env, Lang, TALcolors
+from TALinputs import TALinput
+
+from scc_lib import *
+
+# aggiorna meta.yaml
+# METADATA OF THIS TAL_SERVICE:
+problem="strongly_connected_components"
+service="eval_find_scc"
+args_list = [
+    ('code_lang',str),
+    ('goal',str),
+    ('lang',str),
+]
+
+ENV =Env(problem, service, args_list)
+TAc =TALcolors(ENV)
+LANG=Lang(ENV, TAc, lambda fstring: eval(f"f'{fstring}'"))
+TAc.print(LANG.opening_msg, "green")
+
+g, graph, edges , m = GenerateGraph()
+TAc.print(f"{graph}", "yellow")
+cfc = g.printSCCs().strip()
+ps = input()
+
+l1 = len(cfc)
+l2 = len(ps)
+sp1=contaspazi(cfc)
+sp2=contaspazi(ps)
+if (l1 != l2 or sp1 != sp2):
+    TAc.print(f"Certificato SBAGLIATO:{cfc}", "green")
+    exit(0)
+for i in cfc.split(' '):
+    a=permuta(i)
+    ris=False
+    for j in ps.split(' '):
+        b=permuta(j)
+        if a == b:
+            ris=True
+    if ris == False:
+        TAc.print(f"Certificato SBAGLIATO:{cfc}", "green")
+        exit(0)
+    if ris==True:
+        TAc.print("\nCertificato ESATTO.", "green")
+        exit(0)
