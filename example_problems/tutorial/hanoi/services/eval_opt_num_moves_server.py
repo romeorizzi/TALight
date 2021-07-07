d9e1c7f82d85500d137f63cf810df95b9910cbb9
envq-laptop <enrico.sgarbanti@gmail.com>
2021-07-07 09:11:44 +0200

    improved eval_opt_num_moves

--- example_problems/tutorial/hanoi/services/eval_opt_num_moves_server.py
+++ example_problems/tutorial/hanoi/services/eval_opt_num_moves_server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from sys import stderr, exit, argv
 import random
+from time import monotonic
 
 from TALinputs import TALinput
 from multilanguage import Env, Lang, TALcolors
@@ -28,8 +29,7 @@ args_list = [
 ENV =Env(problem, service, args_list)
 TAc =TALcolors(ENV)
 LANG=Lang(ENV, TAc, lambda fstring: eval(f"f'{fstring}'"))
-if not ENV['silent']:
-    TAc.print(LANG.opening_msg, "green")
+TAc.print(LANG.opening_msg, "green")
 
 
 # INITIALIZATION
@@ -47,51 +47,60 @@ hanoi = HanoiTowerProblem(ENV['v'])
 # START TESTS
 num_tests = ENV['num_tests']
 n_max = ENV['n_max']
-successes = 0
-fails = 0
 if ENV['start'] != 'general' and ENV['final'] != 'general':
     # in this case the test are equal, so num_tests must be 1
     num_tests = 1
+
+def one_test(t, n):
+    # get type of configurations
+    start = get_input_from(ENV['start'], n, seed, t)
+    final = get_input_from(ENV['final'], n, seed, t + 1)
+    TAc.print(LANG.render_feedback("print-configs", f"{start}\n{final}"), "green", ["bold"])
+    
+    # Get the correct solution
+    modulus = ENV['ok_if_congruent_modulus']
+    opt_sol = hanoi.getMinMoves(start, final)
+    if modulus != 0:
+        overflow = (opt_sol >= modulus)
+        mod_sol = opt_sol % modulus
+
+    # Get user solution
+    t_start = monotonic()
+    user_sol, = TALinput(int, TAc=TAc)
+    t_end = monotonic()
+    time_user = t_end - t_start # seconds in float
+    
+    # check the user solution
+    if modulus == 0 or not overflow: #case: not modulus or modulus irrilevant
+        if user_sol != opt_sol:
+            return False, (start, final)
+
+    else: # case: modulus
+        if user_sol != mod_sol:
+            return False, (start, final)
+
+    return True, time_user
+
+
+# Execute all test
+# avg_time = 0
 for t in range(1, 2*num_tests, 2):
     for n in range(1, n_max + 1):
-        # get type of configurations
-        start = get_input_from(ENV['start'], n, seed, t)
-        final = get_input_from(ENV['final'], n, seed, t + 1)
-        TAc.print(LANG.render_feedback("print-configs", f"{start}\n{final}"), "green", ["bold"])
-        
-        # Get the correct solution
-        modulus = ENV['ok_if_congruent_modulus']
-        opt_sol = hanoi.getMinMoves(start, final)
-        if modulus != 0:
-            overflow = (opt_sol >= modulus)
-            mod_sol = opt_sol % modulus
+        success, info = one_test(t, n)
+        if success:
+            TAc.print(LANG.render_feedback("success", "# success"), "green", ["bold"])
+            # i = t * n_max + n
+            # avg_time = (avg_time * i + info) / (i + 1) 
 
-        # Get user solution
-        user_sol, = TALinput(int, TAc=TAc)
-        
-        # check the user solution
-        if modulus == 0 or not overflow: #case: not modulus or modulus irrilevant
-            if user_sol == opt_sol:
-                successes += 1
-                TAc.print(LANG.render_feedback("success", "# success"), "green", ["bold"])
-            else:
-                fails += 1
-                TAc.print(LANG.render_feedback("fail", "# fail"), "red", ["bold"])
-
-        else: # case: modulus
-            if user_sol == mod_sol:
-                successes += 1
-                TAc.print(LANG.render_feedback("success", "# success"), "green", ["bold"])
-            else:
-                fails += 1
-                TAc.print(LANG.render_feedback("fail", "# fail"), "red", ["bold"])
+            # if abs(info[0] - info[1]) < 0.2:
+            #     TAc.print(LANG.render_feedback("not_efficient", f"# not efficient {info[0]} vs {info[1]}"), "red", ["bold"])
+            #     break
+        else:
+            TAc.print(LANG.render_feedback("fail", "# fail with {info}"), "red", ["bold"])
+            break
 
 
 # END
 TAc.print(LANG.render_feedback("end", "end"), "green", ["bold"])
-total = num_tests * n_max
-assert total == successes + fails
-TAc.print(LANG.render_feedback("stat-successes", f"# success:  {successes}/{total}"), "green", ["bold"])
-TAc.print(LANG.render_feedback("stat-fails",     f"# fails:    {fails}/{total}"), "red", ["bold"])
 
 exit(0)
