a180cb9824cd21bf61cb9c4b103119253bc5d108
envq-laptop <enrico.sgarbanti@gmail.com>
2021-08-01 18:59:26 +0200

    added create_books

---
+++ example_problems/tutorial/hanoi/services/create_books.sh
@@ -0,0 +1,41 @@
+#!/bin/zsh
+
+PATH_TO_EXE=$HOME/github/TALight/TAL_utils/problem_maker/python/multilanguage
+PATH_TO_PREAMBLE=$HOME/github/TALight/TAL_utils/problem_maker/python/multilanguage
+
+
+# REMOVE BOOKS
+if [ "$1" = "clean" ]; then
+    rm *en.yaml
+fi
+
+
+# CREATE BOOKS
+if [ "$1" = "en" ]; then
+    # get _server.py files
+    files=(`find . -type f -iname "*_server.py"`)
+    for entry in $files
+    do
+        file="${entry:2:-10}"
+        if [ -f ${file}_feedbackBook.en.yaml ]; then
+            echo "WARNING: ${file}_feedbackBook.en.yaml  already exists."
+        else
+            $PATH_TO_EXE/extract_feedbackBook_hardcoded.py ${file}_server.py fstring_count -preamble_file=$PATH_TO_PREAMBLE/preamble.en.yaml 1> ${file}_feedbackBook.en.yaml 
+        fi
+    done
+fi
+
+
+if [ "$1" = "it" ]; then
+    # get _server.py files
+    files=(`find . -type f -iname "*_server.py"`)
+    for entry in $files
+    do
+        file="${entry:2:-10}"
+        if [ -f ${file}_feedbackBook.en.yaml ]; then
+            echo "WARNING: ${file}_feedbackBook.it.yaml  already exists."
+        else
+            $PATH_TO_EXE/extract_feedbackBook_hardcoded.py ${file}_server.py fstring_count -preamble_file=$PATH_TO_PREAMBLE/preamble.it.yaml 1> ${file}_feedbackBook.it.yaml 
+        fi
+    done
+fi
