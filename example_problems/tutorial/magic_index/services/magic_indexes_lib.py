17b208a3272ca9f545aa27b507d852108c930cb3
Luca Marzari <luca@pop-os.localdomain>
2021-11-05 12:34:06 +0100

    started first part play1 server

--- example_problems/tutorial/magic_index/services/magic_indexes_lib.py
+++ example_problems/tutorial/magic_index/services/magic_indexes_lib.py
@@ -1,10 +1,8 @@
+#!/usr/bin/env python3
 import random, math
+import numpy as np
 
-#TO DO: 
-#       - uniformare la funzione per sia generare il vettore di domande per il caso random e pessimo (gioco in modalità optimal) 
-#       sia per controllare il numero di domande minimo da fare nel caso scelto check_n_questions_random_case = num_questions_worst_case. 
-#       - testare tutto che funzioni nel caso di giocata random --> optmal necessità di sistemare il discorso della generazione del vettore tramite range dei seed prestabiliti.
-
+ 
 def check_input_vector(vec, TAc, LANG):
     for i in range(1,len(vec)):
         if vec[i] == vec[i-1]:
@@ -74,6 +72,7 @@ def num_questions_worst_case_support(n):
     else:
         return 1 + num_questions_worst_case_support((n-1)//2 + ((n-1)%2))
 
+'''
 questions = 0
 def generate_optimal_question_vector(vec, lower, upper, vector_optmal_questions):
     global questions
@@ -93,6 +92,7 @@ def generate_optimal_question_vector(vec, lower, upper, vector_optmal_questions)
 
     if vec[mid] == mid: 
         return generate_optimal_question_vector(vec, lower, mid - 1, vector_optmal_questions) + [mid] + generate_optimal_question_vector(vec, mid + 1, upper, vector_optmal_questions)
+'''
 
 
 def print_vector(vec, TAc, LANG):
@@ -114,10 +114,213 @@ def print_vector(vec, TAc, LANG):
     print()
 
 
+table_g = [None] * 1000
+table_f = [None] * 1000
+worst_f, worst_g = {}, {}
+table_f[0],table_g[0] = 0, 0
+table_f[1], table_g[1] = 1, 1
 
-# remove ??
-def check_n_questions_random_case(questions_vector):
-    return len([i for i in questions_vector if i is not None])
+
+def g(u, case):
+    global table_g, worst_g
+    print(case)
+    # arrotondiamo per eccesso per non avere il problema della numerazione degli indici da 0 o 1
+    pos = math.ceil(u/2)
+    if u == 0:
+        return table_g[u]
+    if u == 1:
+        return table_g[u]
+    else:
+        if table_g[pos-1] == None:
+            table_g[pos-1] = g(pos-1, case) #-1 or 1
+        case1 = table_g[pos-1]
+
+        if table_g[u-pos] == None:
+            table_g[u-pos] = g(u-pos,case)
+        case0 = table_g[u-pos] #0
+
+        #if the worst case is the same choose a random value between 0 and 1 (-1)
+        if case0 == case1:
+            w = random.randint(0,1)
+        else:
+            w = np.argmax([case1, case0])
+
+        if w == 0:
+            if case == 'left':
+                worst_g[pos-1] = '-1'
+            else:
+                worst_g[pos-1] = '1'
+        else:
+            worst_g[pos-1] = '0'
+        
+        return 1 + max(case1, case0)
+  
+# la funzione f dato l'indice ottimale (quello spezzando al centro) torna quante domande bisogna fare nel caso pessimo per ogni possibile valore -1 (valore più piccolo dell'indice), 
+# 0 valore identico all'indice (MI), e infine 1 (valore più grande dell'indice) 
+def f(u):
+    global table_f, table_g, worst_f
+    # arrotondiamo per eccesso per non avere il problema della numerazione degli indici da 0 o 1
+    pos = math.ceil(u/2)
+    if u == 0:
+        return table_f[u]
+    if u == 1:
+        return table_f[u]
+    else:
+        if table_f[u-pos] == None:
+            table_f[u-pos] = f(u-pos) #-1
+        case1 = table_f[u-pos]
+
+        if table_f[pos - 1] == None:
+            table_f[pos - 1] = f(pos-1)
+        case2 = table_f[pos - 1] #1
+
+        if table_g[pos-1] == None:
+            table_g[pos-1] = g(pos-1, 'left')
+        if table_g[u-pos] == None:
+            table_g[u-pos] = g(u-pos, 'right') 
+
+        case0 =  table_g[pos-1] + table_g[u-pos] #0
+
+        w = np.argmax([case1,case2,case0])
+        if w == 0:
+            worst_f[pos-1] = '-1'
+        elif w == 1:
+            worst_f[pos-1] = '1'
+        else:
+            worst_f[pos-1] = '0'
+     
+        return 1 + max(case1, case2, case0)
+
+
+def get_positions_f(vec):
+    # get last occurence of a '-1', index() returns the first occurence so we inverted the vector and took the first occurence (so the last in the original vec) and subtract the result from the original last position of the vector. For the computation of the optimal move, we use the rounded down division in order to avoid mistakes with indexes. Note for the optimal move with enumeration that starts from 1 we use the following formula: (unknow / 2) rounded up, which corresponds to the formula: (lower+upper)// 2 (rounded down) with enumeration from 0.
+    if '-1' in vec and '1' in vec:
+        posLess = (len(vec)-1) - vec[::-1].index('-1')
+        posGreater = vec.index('1')
+
+        if posGreater == posLess + 1:
+            return len(vec[posLess+1:posGreater]), None
+        return len(vec[posLess+1:posGreater]), ((posLess+1) + (posGreater-1))//2
+    elif '-1' in vec: 
+        posLess = (len(vec)-1) - vec[::-1].index('-1')
+        if posLess == len(vec)-1:
+            return 0,None
+        else:
+            return len(vec[posLess+1:]), (((posLess+1)+ (len(vec)-1))//2)
+    elif '1' in vec:
+        posGreater = vec.index('1')
+        if posGreater == 0:
+            return 0,None
+        else:
+            return len(vec[:posGreater]), ((posGreater-1)//2)
+    else:
+        return len(vec), ((len(vec)-1)//2)
+    
+def get_positions_g(vec):
+    first_occ_0 = vec.index('0')
+    last_occ_0 = (len(vec)-1) - vec[::-1].index('0')
+
+    last_occ_less = 0
+    first_occ_greater = len(vec) # we initialize this like if the first occurence was out of the vector
+    sum_factor = 0 # use if there is not a '-1' in the vector 
+    sub_factor = 0 # use if there is not a '1' in the vector 
+    s = 0
+   
+    # if it exists at least one element equal to '-1' in vec update the position of '-1' with the last occurence.
+    if '-1' in vec:
+        last_occ_less = (len(vec)-1) - vec[::-1].index('-1')
+        sum_factor += 1
+    
+    # if it exists at least one element equal to '1' in vec update the position of '-1' with the first occurence.
+    if '1' in vec:
+        first_occ_greater = vec.index('1')
+        sub_factor += 1
+
+    # use this variable in the case where we don't have a 1 in vec. Since we initialize the first occurance to len(vec), then in the division if we don't subtract a -1 we obtain a wrong position where to point
+    if first_occ_greater == len(vec):
+        s += 1
+
+    #print(vec)
+    #print(f'first_occ_0 = {first_occ_0}, last_occ_0 = {last_occ_0}, last_occ_less = {last_occ_less}, first_occ_greater = {first_occ_greater}')
+
+
+    # the optimal moves are computed as the middle value between the last occurence of a '-1' and the first MI excluded and the last occurence of MI excluded and the first occurence of a '1' 
+    optimal_pos = [((last_occ_less + sum_factor) + (first_occ_0 - 1))//2 , ((last_occ_0 + 1) + (first_occ_greater - s - sub_factor))//2]    
+    unknowns = [len(vec[last_occ_less + sum_factor: first_occ_0]), len(vec[last_occ_0 + 1: first_occ_greater])]
+
+    # check that if unknow is 0 in some part of the vector (i.e., we know already everything), the optimal move is None.
+    if unknowns[0] == 0:
+        optimal_pos[0] = None
+    if unknowns[1] == 0:
+        optimal_pos[1] = None    
+
+    return unknowns, optimal_pos
+
+
+def getWorst_f():
+    return worst_f
+
+def getWorst_g():
+    return worst_g
+
+def cleanWorst_f():
+    worst_f = {}
+    return
+
+def cleanWorst_g():
+    worst_g = {}
+    return
+
+
+
+def generate_value_for_vector(server_vector, discovered_vec, chosen_index):
+    if server_vector[chosen_index] == '0':
+        return chosen_index
+    elif server_vector[chosen_index] == '-1':
+
+        # we search the last '-1' index in the server_vec if exists.
+        if '-1' in server_vector[:chosen_index]:
+            index_small_value = (len(server_vector[:chosen_index])-1) - server_vector[chosen_index-1::-1].index('-1')
+            lower_bound = discovered_vec[index_small_value]
+
+        else:
+            lower_bound = chosen_index - 1 - 100
+
+        # we check if in the previous iteration we discovered another smaller value ('-1') in the right side of the chosen index in order to take that value as possible upper bound.
+        possible_upper_bound = None
+        if '-1' in server_vector[chosen_index+1:]:
+            first_bigger_small = server_vector[chosen_index+1:].index('-1') + 1
+            print(first_bigger_small)
+            possible_upper_bound = discovered_vec[first_bigger_small]
+
+        upper_bound = chosen_index-1 
+        if possible_upper_bound != None and upper_bound > possible_upper_bound-1:
+            upper_bound = possible_upper_bound-1
+        
+
+    elif server_vector[chosen_index] == '1':
+
+        # we search the last '-1' index in the server_vec if exists.
+        if '1' in server_vector[chosen_index+1:]:
+            index_greater_value = server_vector[chosen_index+1:].index('1')
+            upper_bound = discovered_vec[index_greater_value]
+
+        else:
+            upper_bound = chosen_index + 1 + 100
+
+        possible_lower_bound = None
+        if '1' in server_vector[:chosen_index]:
+            first_previous_greater = (len(server_vector[:chosen_index])-1) - server_vector[chosen_index-1::-1].index('1')
+            possible_lower_bound = discovered_vec[first_previous_greater]
+
+        lower_bound = chosen_index+1
+        if possible_lower_bound != None and lower_bound < possible_lower_bound+1:
+            lower_bound = possible_lower_bound+1
+
+
+    new_value = random.randint(lower_bound, upper_bound)
+    return new_value
+
 
 def check_goal(opponent, goal, feedback, magic_indexes, user_solution, vector_optmal_questions, vector_questions, wasted_dollars, min_questions_worst_case, TAc, LANG):
     # we give feedback based on the chosen optimality level
--- example_problems/tutorial/magic_index/services/play1_server.py
+++ example_problems/tutorial/magic_index/services/play1_server.py
@@ -9,7 +9,7 @@ from magic_indexes_lib import *
 
 # METADATA OF THIS TAL_SERVICE:
 args_list = [
-    #('n',str),
+    ('n',str),
     ('opponent',str),
     ('goal',str),
     ('feedback',str),
@@ -19,68 +19,79 @@ TAc =TALcolors(ENV)
 LANG=Lang(ENV, TAc, lambda fstring: eval(f"f'{fstring}'"))
 
 # START CODING YOUR SERVICE:
+vector_len = int(ENV['n'])
 
-if ENV['opponent'] == 'optimal':
-    # TO DO: change this function with random_vector() after the implementation with different seed
-    vec,_, size = random_vector()
-elif ENV['opponent'] == 'random':
-    vec, _ , size = random_vector()
+# this vector contains -1,0,1 and it's filled by the server with the worst case scenario and it's never shown to the user during the game.
+server_vector = [None] * vector_len
 
-vector_optmal_questions = [None] * len(vec)
-# generate_optimal_question_vector fills the vector with the optimal questions and returns the list of magic indexes
-magic_indexes = generate_optimal_question_vector(vec, 0, len(vec)-1, vector_optmal_questions)
-min_questions = len([i for i in vector_optmal_questions if i != None])
-
-
+# this variable contains the questions made by the user during the game
 wasted_dollars = 0
-#magic_indexes = spot_magic_index(vec)
-print(vec, vector_optmal_questions, min_questions, num_questions_worst_case(len(vec)))
 
-discovered_vec = ['?' for _ in range(1, size+1)] # print vector
-vector_questions = [None for i in range(1,len(vec)+1)] # support vector: it stores the queried values in the asked order
+# this is the vector discovered/generated during the game
+discovered_vec = ['?' for _ in range(1, vector_len+1)] # print vector
+
+# this boolean variable is initialized to True in case ENV['feedback'] == 'spot_first_gift'
 firstGift = True
 
-TAc.print(LANG.render_feedback("random_vector", f'I generated a vector of size: {size}.'), "yellow", ["bold"])
+TAc.print(LANG.render_feedback("random_vector", f'All right, let us play on a vector of size: {vector_len}.'), "yellow", ["bold"])
+print_vector(discovered_vec, TAc, LANG)
 
 while True:
     TAc.print(LANG.render_feedback("random_vector", f'Choose an index to play:'), "yellow", ["bold"])
-    n = TALinput(str, num_tokens=1, regex="^(0|[1-9][0-9]{0,5})$", regex_explained="enter an index n to check the value at that position", TAc=TAc, LANG=None)
-    n = int(n[0])
-    if n >= size:
-        TAc.print(LANG.render_feedback("error", f'The input value for the index is not between 0 and {size-1}. Insert another index to play:'), "red", ["bold"])
+    chosen_index = TALinput(str, num_tokens=1, regex="^(0|[1-9][0-9]{0,5})$", regex_explained="Enter an index to discover the vector value", TAc=TAc, LANG=None)
+    chosen_index = int(chosen_index[0])
+
+    if chosen_index >= vector_len:
+        TAc.print(LANG.render_feedback("error", f'The input value for the index is not between 0 and {vector_len-1}. Insert another index to play:'), "red", ["bold"])
     else:
-        discovered_vec[n] = vec[n]
-        vector_questions[n] = wasted_dollars 
-        print_vector(discovered_vec, TAc, LANG)
+        if ENV['opponent'] == 'optimal':
+            if '0' not in server_vector:
+                unknown, optimal_pos = get_positions_f(server_vector)
+                if chosen_index == optimal_pos:
+                    # we want to know which is the value to insert in server_vector[optimal_pos]
+                    # we take the value computed in the function f, taking the greatest key of the dictionary and the corresponding value
+                    _ = f(unknown)
+                    server_vector[optimal_pos] = worst_f[max(worst_f)]
+                    #and we clean the support dictionary
+                    cleanWorst_f()
+                    
+                    # we generate the value for the discovered_vec based on the just computed value in f()
+                    discovered_vec[chosen_index] = generate_value_for_vector(server_vector, discovered_vec, chosen_index) 
+
+
+       
+            print_vector(discovered_vec, TAc, LANG)
+            
+            # check if the user is playing (optimally) according to the chosen level
+            if ENV['feedback'] == 'spot_first_gift' and firstGift:
+                if vector_optmal_questions[n] != vector_questions[n]:
+                    TAc.print(LANG.render_feedback("first error", f'# Here you made your first mistake!'), "yellow", ["bold"])
+                    firstGift = False
+            elif ENV['feedback'] == 'spot_every_gift':
+                if vector_optmal_questions[n] != vector_questions[n]:
+                    TAc.print(LANG.render_feedback("error", f'# Here you made a mistake!'), "yellow", ["bold"])
+
+            wasted_dollars += 1
+
+            # we ask if the user wants to try to give the solution
+            TAc.print(LANG.render_feedback("solution proposed", f'Do you want to give the solution? (y for yes, otherwise any other character)'), "yellow", ["bold"])
+            ans = TALinput(str, num_tokens=1, regex_explained="enter y if you want submit your solution, otherwise any other character", TAc=TAc, LANG=None)
+            
+            if ans[0].lower() == 'y':
+                TAc.print(LANG.render_feedback("solution proposed", f'Enter your vector of magic indexes here: (example: 1,2,3)'), "yellow", ["bold"])
+                user_solution = TALinput(str, num_tokens=1, regex="^((0|-{0,1}[1-9][0-9]{0,3})(,(0|-{0,1}[1-9][0-9]{0,3})){0,1000})|e$", regex_explained="regex for magic indexes provided by the user", TAc=TAc, LANG=None)
+                user_solution = user_solution[0].split(',')
+
+                if user_solution != ['e']:
+                    user_solution = list(map(int, user_solution))
+                    check_input_vector(user_solution, TAc, LANG)
+
+                check_goal(ENV['opponent'], ENV['goal'], ENV['feedback'], magic_indexes, user_solution, vector_optmal_questions, vector_questions, wasted_dollars, min_questions, TAc, LANG)
+            else:
+               TAc.print(LANG.render_feedback("the game continues", f'Perfect! Let us keep playing, you have done {wasted_dollars} questions so far...'), "yellow", ["bold"])
         
-        # check if the user is playing (optimally) according to the chosen level
-        if ENV['feedback'] == 'spot_first_gift' and firstGift:
-            if vector_optmal_questions[n] != vector_questions[n]:
-                TAc.print(LANG.render_feedback("first error", f'# Here you made your first mistake!'), "yellow", ["bold"])
-                firstGift = False
-        elif ENV['feedback'] == 'spot_every_gift':
-            if vector_optmal_questions[n] != vector_questions[n]:
-                TAc.print(LANG.render_feedback("error", f'# Here you made a mistake!'), "yellow", ["bold"])
-
-        wasted_dollars += 1
-
-        # we ask if the user wants to try to give the solution
-        TAc.print(LANG.render_feedback("solution proposed", f'Do you want to give the solution? (y for yes, otherwise any other character)'), "yellow", ["bold"])
-        ans = TALinput(str, num_tokens=1, regex_explained="enter y if you want submit your solution, otherwise any other character", TAc=TAc, LANG=None)
-        
-        if ans[0].lower() == 'y':
-            TAc.print(LANG.render_feedback("solution proposed", f'Enter your vector of magic indexes here: (example: 1,2,3)'), "yellow", ["bold"])
-            user_solution = TALinput(str, num_tokens=1, regex="^((0|-{0,1}[1-9][0-9]{0,3})(,(0|-{0,1}[1-9][0-9]{0,3})){0,1000})|e$", regex_explained="regex for magic indexes provided by the user", TAc=TAc, LANG=None)
-            user_solution = user_solution[0].split(',')
-
-            if user_solution != ['e']:
-                user_solution = list(map(int, user_solution))
-                check_input_vector(user_solution, TAc, LANG)
-
-            check_goal(ENV['opponent'], ENV['goal'], ENV['feedback'], magic_indexes, user_solution, vector_optmal_questions, vector_questions, wasted_dollars, min_questions, TAc, LANG)
-        else:
-            TAc.print(LANG.render_feedback("the game continues", f'Perfect! Let us keep playing, you have done {wasted_dollars} questions so far...'), "yellow", ["bold"])
+        elif ENV['opponent'] == 'random':
+            print()
 
 
 #TODO: chiedere di controllare il discorso della chiusura del server(?) se non arriva input da linea di comando dopo un tot.
-# chiedere cosa deve fare eval_play1
