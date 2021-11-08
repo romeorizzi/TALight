4f088a6e75aab9fc01ba6c83bc79e76bbd2351c5
Romeo Rizzi <romeo.rizzi@univr.it>
2021-11-08 08:58:45 +0100

    commit pre-conflict management

--- example_problems/tutorial/insertion_sort/bots/template_lib.py
+++ example_problems/tutorial/insertion_sort/bots/template_lib.py
@@ -7,21 +7,21 @@ class InsertionSortMachine:
         self.tmp_buffer = None
         self.working_array = []
 
-    def console(self, log_msg, wait_for_recipt: bool, expected_log: str = None):
+    def console(self, log_msg, wait_for_receipt: bool, expected_log: str = None):
         assert expected_log == None or log_msg == expected_log
         print(log_msg)
-        if wait_for_recipt:
+        if wait_for_receipt:
             print("Insert a feedback line (or just press RETURN) to continue ...")
             input()
             
-    def load_next_input_element_in_tmp_buffer(self, val: int, wait_for_recipt: bool = True, expected_log: str = None):
+    def load_next_input_element_in_tmp_buffer(self, val: int, wait_for_receipt: bool = True, expected_log: str = None):
         if self.tmp_buffer is not None:
             print(
                 "Ahi, my problem-solver bot is overwriting a yet unflushed value stored in the tmp_buffer. This is going to erase information which will be definitely lost it in the Insertion Sort algorithm approach.")
         self.tmp_buffer = val
-        self.console(f"#LOG_load_next_input_element_in_tmp_buffer (got {val})", wait_for_recipt, expected_log)
+        self.console(f"#LOG_load_next_input_element_in_tmp_buffer (got {val})", wait_for_receipt, expected_log)
 
-    def flush_tmp_buffer_on_pos(self, i: int, wait_for_recipt: bool = True, expected_log: str = None):
+    def flush_tmp_buffer_on_pos(self, i: int, wait_for_receipt: bool = True, expected_log: str = None):
         if self.tmp_buffer is None:
             print(
                 "Ahi, my problem-solver bot is flushing an empty tmp_buffer. I expect complaints from the checking server.")
@@ -29,9 +29,9 @@ class InsertionSortMachine:
             self.working_array.append(None)
         self.working_array[i] = self.tmp_buffer
         self.tmp_buffer = None
-        self.console(f"#LOG_flush_tmp_buffer_on_pos {i}", wait_for_recipt, expected_log)
+        self.console(f"#LOG_flush_tmp_buffer_on_pos {i}", wait_for_receipt, expected_log)
 
-    def clone_to_its_right_ele_in_pos(self, i: int, wait_for_recipt: bool = True, expected_log: str = None):
+    def clone_to_its_right_ele_in_pos(self, i: int, wait_for_receipt: bool = True, expected_log: str = None):
         if not 0 <= i < len(self.working_array):
             print(
                 f"Ahi, my problem-solver bot is asking to clone the element in pos {i} which does not exists since {i} is not in the interval [0,{len(self.working_array)}).")
@@ -39,9 +39,9 @@ class InsertionSortMachine:
             self.working_array.append(self.working_array[i])
         else:
             self.working_array[i + 1] = self.working_array[i]
-        self.console(f"#LOG_clone_to_its_right_ele_in_pos {i}", wait_for_recipt, expected_log)
+        self.console(f"#LOG_clone_to_its_right_ele_in_pos {i}", wait_for_receipt, expected_log)
 
-    def what_in_tmp_buffer_goes_before_than_what_in_pos(self, i: int, wait_for_recipt: bool = True, expected_log: str = None):
+    def what_in_tmp_buffer_goes_before_than_what_in_pos(self, i: int, wait_for_receipt: bool = True, expected_log: str = None):
         if self.tmp_buffer is None:
             print(
                 "Ahi, my problem-solver bot asks to compare with others the element contained in the tmp_buffer, but this buffer is empty. I expect complaints from the checking server.")
@@ -51,13 +51,13 @@ class InsertionSortMachine:
                 f"Ahi, my problem-solver bot asks to compare with others the element contained in position {i} of the current working array. However, no element has ever been placed in this position of the array. I expect complaints from the checking server.")
             return False
         if self.tmp_buffer < self.working_array[i]:
-            self.console(f"#LOG_compare_what_in_tmp_buffer_with_what_in_pos {i} (<)", wait_for_recipt, expected_log)
+            self.console(f"#LOG_compare_what_in_tmp_buffer_with_what_in_pos {i} (<)", wait_for_receipt, expected_log)
             return True
-        self.console(f"#LOG_compare_what_in_tmp_buffer_with_what_in_pos {i} (>=)", wait_for_recipt, expected_log)
+        self.console(f"#LOG_compare_what_in_tmp_buffer_with_what_in_pos {i} (>=)", wait_for_receipt, expected_log)
         return False
 
-    def output_final_array(self, wait_for_recipt: bool = True, expected_log: str = None):
-        self.console(f"#LOG_output_final_array ({len(self.working_array)}: {' '.join(map(str,self.working_array))})", wait_for_recipt, expected_log)
+    def output_final_array(self, wait_for_receipt: bool = True, expected_log: str = None):
+        self.console(f"#LOG_output_final_array ({len(self.working_array)}: {' '.join(map(str,self.working_array))})", wait_for_receipt, expected_log)
 
 
 if __name__ == "__main__":
@@ -83,21 +83,21 @@ if __name__ == "__main__":
     ]
 
     SM = InsertionSortMachine()
-    SM.load_next_input_element_in_tmp_buffer(12, wait_for_recipt=False, expected_log=expected_logs[0])
-    SM.flush_tmp_buffer_on_pos(0, wait_for_recipt=False, expected_log=expected_logs[1])
-    SM.load_next_input_element_in_tmp_buffer(11, wait_for_recipt=False, expected_log=expected_logs[2])
-    SM.what_in_tmp_buffer_goes_before_than_what_in_pos(0, wait_for_recipt=False, expected_log=expected_logs[3])
-    SM.clone_to_its_right_ele_in_pos(0, wait_for_recipt=False, expected_log=expected_logs[4])
-    SM.flush_tmp_buffer_on_pos(0, wait_for_recipt=False, expected_log=expected_logs[5])
-    SM.load_next_input_element_in_tmp_buffer(15, wait_for_recipt=False, expected_log=expected_logs[6])
-    SM.what_in_tmp_buffer_goes_before_than_what_in_pos(1, wait_for_recipt=False, expected_log=expected_logs[7])
-    SM.flush_tmp_buffer_on_pos(2, wait_for_recipt=False, expected_log=expected_logs[8])
-    SM.load_next_input_element_in_tmp_buffer(13, wait_for_recipt=False, expected_log=expected_logs[9])
-    SM.what_in_tmp_buffer_goes_before_than_what_in_pos(2, wait_for_recipt=False, expected_log=expected_logs[10])
-    SM.clone_to_its_right_ele_in_pos(2, wait_for_recipt=False, expected_log=expected_logs[11])
-    SM.what_in_tmp_buffer_goes_before_than_what_in_pos(1, wait_for_recipt=False, expected_log=expected_logs[12])
-    SM.flush_tmp_buffer_on_pos(2, wait_for_recipt=False, expected_log=expected_logs[13])
-    SM.output_final_array(wait_for_recipt=False, expected_log=expected_logs[14])
+    SM.load_next_input_element_in_tmp_buffer(12, wait_for_receipt=False, expected_log=expected_logs[0])
+    SM.flush_tmp_buffer_on_pos(0, wait_for_receipt=False, expected_log=expected_logs[1])
+    SM.load_next_input_element_in_tmp_buffer(11, wait_for_receipt=False, expected_log=expected_logs[2])
+    SM.what_in_tmp_buffer_goes_before_than_what_in_pos(0, wait_for_receipt=False, expected_log=expected_logs[3])
+    SM.clone_to_its_right_ele_in_pos(0, wait_for_receipt=False, expected_log=expected_logs[4])
+    SM.flush_tmp_buffer_on_pos(0, wait_for_receipt=False, expected_log=expected_logs[5])
+    SM.load_next_input_element_in_tmp_buffer(15, wait_for_receipt=False, expected_log=expected_logs[6])
+    SM.what_in_tmp_buffer_goes_before_than_what_in_pos(1, wait_for_receipt=False, expected_log=expected_logs[7])
+    SM.flush_tmp_buffer_on_pos(2, wait_for_receipt=False, expected_log=expected_logs[8])
+    SM.load_next_input_element_in_tmp_buffer(13, wait_for_receipt=False, expected_log=expected_logs[9])
+    SM.what_in_tmp_buffer_goes_before_than_what_in_pos(2, wait_for_receipt=False, expected_log=expected_logs[10])
+    SM.clone_to_its_right_ele_in_pos(2, wait_for_receipt=False, expected_log=expected_logs[11])
+    SM.what_in_tmp_buffer_goes_before_than_what_in_pos(1, wait_for_receipt=False, expected_log=expected_logs[12])
+    SM.flush_tmp_buffer_on_pos(2, wait_for_receipt=False, expected_log=expected_logs[13])
+    SM.output_final_array(wait_for_receipt=False, expected_log=expected_logs[14])
 
 
     print("\nfurther tests ...\n")
--- example_problems/tutorial/insertion_sort/bots/test_bot.py
+++ example_problems/tutorial/insertion_sort/bots/test_bot.py
@@ -5,14 +5,14 @@ from time import sleep
 from template_lib import InsertionSortMachine
 
 usage=f"""Call me like this:
-   > {argv[0]}  [ <input_filename> [wait_for_recipt] ]
+   > {argv[0]}  [ <input_filename> [wait_for_receipt] ]
 
    The two (optional) arguments are as follows:
    <input_filename> provides the fullname of a file where the input is stored. This file comprises two lines:
        -the first line contains the number <n> of integers to be sorted. 
        -the second line contains <n> integer numbers separated by spaces.
 
-   [wait_for_recipt] whenever you provide a second argument (its face value is not even checked) then the InsertionSort bot will wait for confirmation at every log step of the InsertionSortMachine it operates.
+   [wait_for_receipt] whenever you provide a second argument (its face value is not even checked) then the InsertionSort bot will wait for confirmation at every log step of the InsertionSortMachine it operates.
 """
 
 #if len(argv) < 2:
@@ -34,19 +34,19 @@ else:
     input_array_of_ints = list(map(int, fin.readline().strip().split()))
     fin.close()
 
-require_recipt = len(argv) > 2
+require_receipt = len(argv) > 2
 
 assert N == len(input_array_of_ints)
 SM = InsertionSortMachine()
 n_ordered = 0
 while n_ordered < N:
-    SM.load_next_input_element_in_tmp_buffer(int(input_array_of_ints[n_ordered]), wait_for_recipt=require_recipt)
+    SM.load_next_input_element_in_tmp_buffer(int(input_array_of_ints[n_ordered]), wait_for_receipt=require_receipt)
     pos_cmp = n_ordered - 1
-    while pos_cmp >= 0 and SM.what_in_tmp_buffer_goes_before_than_what_in_pos(pos_cmp, wait_for_recipt=require_recipt):
-        SM.clone_to_its_right_ele_in_pos(pos_cmp, wait_for_recipt=require_recipt)
+    while pos_cmp >= 0 and SM.what_in_tmp_buffer_goes_before_than_what_in_pos(pos_cmp, wait_for_receipt=require_receipt):
+        SM.clone_to_its_right_ele_in_pos(pos_cmp, wait_for_receipt=require_receipt)
         pos_cmp -= 1
-    SM.flush_tmp_buffer_on_pos(pos_cmp + 1, wait_for_recipt=require_recipt)
+    SM.flush_tmp_buffer_on_pos(pos_cmp + 1, wait_for_receipt=require_receipt)
     n_ordered += 1
-SM.output_final_array(wait_for_recipt=require_recipt)
+SM.output_final_array(wait_for_receipt=require_receipt)
 
 sleep(1)
