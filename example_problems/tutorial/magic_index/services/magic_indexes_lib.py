67ebe70034e0c621fbcb8d82fce567bd74eae0e2
Luca Marzari <luca@pop-os.localdomain>
2021-11-08 17:10:26 +0100

    code refactor

--- example_problems/tutorial/magic_index/services/magic_indexes_lib.py
+++ example_problems/tutorial/magic_index/services/magic_indexes_lib.py
@@ -13,66 +13,12 @@ def check_input_vector(vec, TAc, LANG):
             TAc.print(LANG.render_feedback("decrease", f'No. Your vector is not incrisingly sorted: vec[{i-1}] = {vec[i-1]} > {vec[i]} = vec[{i}].'), "red", ["bold"])
             exit(0)
 
-def random_vector():
-    n = random.randint(5, 20)
-    vec, seed = random_vector_fixed(n)
-    return vec, seed, n
-
-
-def random_vector_fixed(n, seed="random_seed"):
-    if seed=="random_seed":
-        seed = random.randrange(100000,1000000)
-    else:
-        seed = int(seed)
-    random.seed(seed)
-    if seed >= 100000:
-        magics_run_length = min(random.randint(0,n),random.randint(0,n))
-        first_magic = random.randint(0,n-magics_run_length)
-        last_magic = first_magic + magics_run_length
-        vec = list(range(n))
-        for i in range(last_magic+1,n):
-            vec[i] = vec[i-1] + random.randint(2, 5)
-        if first_magic > last_magic:
-            first_magic = last_magic +1
-        for i in range(first_magic-1,-1,-1):
-            vec[i] = vec[i+1] - random.randint(2, 5)
-    else:
-        prefix = (seed%2 == 1) # gli indici magici sono un prefisso o un suffisso?
-        seed_rem=seed//2
-        magics_run_length = seed_rem % (n+1)
-        seed_rem=seed_rem//(n+1)
-        vec = list(range(magics_run_length))
-        base = magics_run_length + 1
-        while len(vec) < n:
-            delta = seed_rem % n
-            seed_rem=seed_rem//n            
-            vec.append(base+delta)
-            base = base+delta+1
-        
-    return vec,seed
-
 def spot_magic_index(vec):
     magic_indexes = []
     for i in range(len(vec)):
         if vec[i]==i:
             magic_indexes.append(i)
     return magic_indexes
-
-# The optimal strategy shoots always in the middle. The worst case is as follows: at the first shoot nature answers "yes, it is a magic index" (otherwise half of the positions are lost from the very beginning). The problem splits into two, left and right, which howevere have a different nature (since I now know that the magic positions will form a prefix or a suffix).
-# On this residual problem nature decides each time I can not split in precisely half, leaving me the biggest half.
-
-def num_questions_worst_case(n):
-    if n==0:
-        return 0
-    else:
-	    return 1 + num_questions_worst_case_support((n-1)//2) + num_questions_worst_case_support((n-1)//2 + ((n-1)%2))
-
-def num_questions_worst_case_support(n):
-    if n==0:
-        return 0
-    else:
-        return 1 + num_questions_worst_case_support((n-1)//2 + ((n-1)%2))
-
 
 def print_vector(vec, TAc, LANG):
     w = len(vec)*2
