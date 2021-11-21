552d9f268262b1ed2460100ab9f8cad98c59a3a3
arma98 <65461205+arma98@users.noreply.github.com>
2021-11-21 17:01:00 +0100

    reduced cases based on player order

--- example_problems/tutorial/chococroc/services/play.py
+++ example_problems/tutorial/chococroc/services/play.py
@@ -17,13 +17,11 @@ args_list = [
     ('value',int)
 ]
 
-ENV =Env(args_list)
+ENV =Env(problem, service, args_list)
 TAc =TALcolors(ENV)
 LANG=Lang(ENV, TAc, lambda fstring: eval(f"f'{fstring}'"))
 
 # START CODING YOUR SERVICE:
-grundy_val = cl.grundy_val(ENV['m'], ENV['n'])
-#print(f"grundy_val={grundy_val}")
 
 m=ENV['m']
 n=ENV['n']
@@ -32,26 +30,26 @@ user_will_lose=False
 
 print_nim=False
 
-if ENV['value']==1:
+if ENV['value'] == 1:
     print_nim=True
 
-if ENV['player']==0 and grundy_val==0:
+if ENV['player'] == 0:
     TAc.print(LANG.render_feedback("user-play-first", f'You will play first.'), "yellow", ["bold"])
-    user_will_lose=True
     
     while m>1 or n>1:
         (m,n)=cl.player_move(m,n,TAc,LANG)
         if print_nim:
             TAc.print(LANG.render_feedback("nim-conf", f'Single NIM tower: {cl.grundy_val(m,n)}.'), "yellow", ["bold"])
-        
-        (m,n)=cl.winning_move(m,n)
-        TAc.print(LANG.render_feedback("pc-move", f'My move: {m} x {n}.'), "yellow", ["bold"])
-        if print_nim:
-            TAc.print(LANG.render_feedback("nim-conf", f'Single NIM tower: {cl.grundy_val(m,n)}.'), "yellow", ["bold"])
-                
-elif ENV['player'] == 1 and grundy_val == 0:
+        user_will_lose=False
+              
+        if m>1 or n>1:        
+            (m,n)=cl.computer_decision_move(m,n,TAc,LANG)
+            if print_nim:
+                TAc.print(LANG.render_feedback("nim-conf", f'Single NIM tower: {cl.grundy_val(m,n)}.'), "yellow", ["bold"])
+            user_will_lose=True
+
+elif ENV['player'] == 1:
     TAc.print(LANG.render_feedback("bot-play-first", f'I will play first.'), "yellow", ["bold"])
-    #user_will_lose=False
     
     while m>1 or n>1:
         (m,n)=cl.computer_decision_move(m,n,TAc,LANG)
@@ -65,40 +63,6 @@ elif ENV['player'] == 1 and grundy_val == 0:
                 TAc.print(LANG.render_feedback("nim-conf", f'Single NIM tower: {cl.grundy_val(m,n)}.'), "yellow", ["bold"])
             user_will_lose=False
 
-elif ENV['player'] == 1 and grundy_val > 0:
-    TAc.print(LANG.render_feedback("bot-play-first", f'I will play first.'), "yellow", ["bold"])
-    user_will_lose=True
-    
-    while m>1 or n>1:
-        (m,n)=cl.winning_move(m, n)
-        TAc.print(LANG.render_feedback("pc-move", f'My move: {m} x {n}.'), "yellow", ["bold"])
-        if print_nim:
-            TAc.print(LANG.render_feedback("nim-conf", f'Single NIM tower: {cl.grundy_val(m,n)}.'), "yellow", ["bold"])
-        
-        if m>1 or n>1:
-            (m,n)=cl.player_move(m,n,TAc,LANG)
-            if print_nim:
-                TAc.print(LANG.render_feedback("nim-conf", f'Single NIM tower: {cl.grundy_val(m,n)}.'), "yellow", ["bold"])
-
-elif ENV['player'] == 0 and grundy_val > 0:
-
-    TAc.print(LANG.render_feedback("user-play-first", f'You will play first.'), "yellow", ["bold"])
-    user_will_lose=False
-    
-    while m>1 or n>1:
-        (m,n)=cl.player_move(m,n,TAc,LANG)
-        if print_nim:
-            TAc.print(LANG.render_feedback("nim-conf", f'Single NIM tower: {cl.grundy_val(m,n)}.'), "yellow", ["bold"])
-        user_will_lose=False
-              
-        if m>1 or n>1:        
-            (m,n)=cl.computer_decision_move(m,n,TAc,LANG)
-            if print_nim:
-                TAc.print(LANG.render_feedback("nim-conf", f'Single NIM tower: {cl.grundy_val(m,n)}.'), "yellow", ["bold"])
-            user_will_lose=True
-                    
-    
-    
 if user_will_lose:
     TAc.print(LANG.render_feedback("pc-win", f'You lose.'), "red", ["bold"])
 
