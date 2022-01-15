786f3f249646ca7c20cedaec2f668efebf11737b
arma98 <65461205+arma98@users.noreply.github.com>
2022-01-15 09:46:37 +0100

    Add files via upload

--- example_problems/tutorial/sum/bots/python/sum_and_difference_mybot.py
+++ example_problems/tutorial/sum/bots/python/sum_and_difference_mybot.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python3
 import math
+from multiprocessing.connection import wait
+from signal import SIGINT, SIGKILL, SIGTERM, signal, SIGPIPE, SIG_DFL
 
 while True:
     spoon = input()
-    if spoon[0] != '#':
-        s,d = map(int, spoon.split() )
-        x1 = (s + d) // 2
-        x2 = (s - d) // 2
-        print(f"{x1} {x2}")
+    if not('WE HAVE FINISHED' in spoon) and not('Correct answers:' in spoon) and not('[Press ENTER to exit]' in spoon):
+        if spoon[0] != '#':
+            s,d = map(int, spoon.split() )
+            x1 = (s + d) // 2
+            x2 = (s - d) // 2
+            print(f"{x1} {x2}")
+        else:
+            splitted=spoon.split()
+            #print(splitted)
+            s,d = map(int, splitted[2:] )
+            x1 = (s + d) // 2
+            x2 = (s - d) // 2
+            print(f"{x1} {x2}")
+    else:
+        print('\0')
+        signal(SIGPIPE,SIG_DFL)
+        signal(SIGINT,SIG_DFL) 
+        exit(0)
+        #while True:
+            #print('\0')
+            #exit(0)
+            #signal(SIGPIPE,SIG_DFL)
+            #signal(SIGKILL,SIG_DFL) 
