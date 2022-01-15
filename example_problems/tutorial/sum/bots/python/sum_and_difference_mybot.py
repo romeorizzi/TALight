fe53f732b031fc9f01feb5775671622820c124a6
Romeo Rizzi <romeo.rizzi@univr.it>
2022-01-15 19:33:12 +0100

    versione dove funzionano i bot BAKsum_and_difference_mybot.py  sum_and_difference_mybot.py

--- example_problems/tutorial/sum/bots/python/sum_and_difference_mybot.py
+++ example_problems/tutorial/sum/bots/python/sum_and_difference_mybot.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python3
 import math
-from signal import SIGINT, SIGKILL, SIGTERM, signal, SIGPIPE, SIG_DFL
 from sys import stderr, exit, argv
 
 while True:
-    print(f"BOT: waiting for input", file=stderr)
     spoon = input()
-    print(f"BOT: spoon={spoon}", file=stderr)
-    if not('WE HAVE FINISHED' in spoon) and not('Correct answers:' in spoon) and not('[Press ENTER to exit]' in spoon):
+    if not 'WE HAVE FINISHED' in spoon:
         if spoon[0] != '#':
             s,d = map(int, spoon.split() )
             x1 = (s + d) // 2
             x2 = (s - d) // 2
             print(f"{x1} {x2}")
-            print(f"BOT: {x1} {x2}", file=stderr)
     else:
-        signal(SIGPIPE,SIG_DFL)
-        signal(SIGINT,SIG_DFL) 
         exit(0)
