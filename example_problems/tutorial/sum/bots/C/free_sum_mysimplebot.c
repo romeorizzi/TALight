62c5f98fba715b62f503a7bd5d7d32587c3d8d77
Dario Ostuni <dario.ostuni@gmail.com>
2022-01-16 12:40:01 +0100

    Fix C version

---
+++ example_problems/tutorial/sum/bots/C/free_sum_mysimplebot.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+int main() {
+    const size_t BUFFER_SIZE = 4096;
+    char buffer[BUFFER_SIZE];
+    int n;
+    setvbuf(stdout, NULL, _IOLBF, BUFFER_SIZE);
+    while(true) {
+        fgets(buffer, BUFFER_SIZE, stdin);
+        fprintf(stderr, "Read %s", buffer);
+        if(strncmp(buffer, "# WE HAVE FINISHED", 18) == 0) {
+            break;
+        } else if(buffer[0] == '#') {
+            continue;
+        } else {
+            sscanf(buffer, "%d", &n);
+            printf("%d 0\n", n);
+        }
+    }
+    return 0;
+}
