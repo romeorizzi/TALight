c1f12dbba089be65408ecca222cd4c44ef9e17c8
arma98 <65461205+arma98@users.noreply.github.com>
2022-02-11 13:17:15 +0100

    Fixed

--- example_problems/tutorial/chococroc/bots/my_chococroc_player.py
+++ example_problems/tutorial/chococroc/bots/my_chococroc_player.py
@@ -7,12 +7,6 @@ BOT = Bot(report_inputs=False,reprint_outputs=False)
 
 while True:
     line = BOT.input()
-    conf_string='It is your turn to move from conf ('
-    if conf_string in line:
-        index_end=line.find(')', len(conf_string))
-        numbers=line[len(conf_string):index_end]
-        s,d = map(int, numbers.split(',') )
-        m,n = cl.computer_move(s,d)
-        print(f"{m} {n}")
-    if line=='You won!' or line=='You lost!':
-        exit(0)
+    cur_m,cur_n = map(int,line.split())
+    m,n=cl.computer_move(cur_m,cur_n)
+    print(f"{m} {n}")
