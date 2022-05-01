1b3d09f953d9be107bb4ca982f5c85297262373d
Nicolò Lutteri <nicololutteri@gmail.com>
2022-05-01 17:28:13 +0200

    Create service file

---
+++ example_problems/tutorial/monitor_grades/services/get_public_records.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env pyhton3
+
+from os import environ, listdir
+from os.path import isfile, isdir, join
+import os
+from FolderData import FolderData
+from FileData import FileData
+from Token import Token
+
+DEBUG = False
+
+if not DEBUG:
+    from multilanguage import Env, Lang, TALcolors
+    from TALfiles import TALfilesHelper
+
+    # METADATA OF THIS SERVICE
+    args_list = [
+        ('problem', str),
+        ('service', str),
+        ('download', int)
+    ]
+
+    ENV = Env(args_list)
+    TAc = TALcolors(ENV)
+    LANG = Lang(ENV, TAc, lambda fstring: eval(f"f'{fstring}'"), print_opening_msg = 'now')
+    TALf = TALfilesHelper(TAc, ENV)
+
+# START CODING YOUR SERVICE:
+
