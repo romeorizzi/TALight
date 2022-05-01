77411d478437978f15d49dd0430e524b514bf326
Nicolò Lutteri <nicololutteri@gmail.com>
2022-05-01 20:53:20 +0200

    Implement public record

--- example_problems/tutorial/monitor_grades/services/get_public_records.py
+++ example_problems/tutorial/monitor_grades/services/get_public_records.py
@@ -29,3 +29,16 @@ if not DEBUG:
 
 # START CODING YOUR SERVICE:
 
+def main(problem : str, service : str, token : str, path : str): 
+    l = LibGrades()
+    l.loadFile(problem, service, token, path)
+
+    if not DEBUG:
+            if ENV['download'] == 1:
+                TALf.str2output_file(l.getProblemList().instanceToString(), "result.csv")
+        
+if __name__ == "__main__":
+    if DEBUG:
+        main("all_problems", "all_service", "123456__RomeoRizzi", os.path.join(os.getcwd(), "log"))
+    else:
+        main(ENV['problem'], ENV['service'], environ['token'], environ['TAL_META_EXP_LOG_DIR'])
