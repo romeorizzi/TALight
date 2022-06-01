40aa0286138ceded8c31be516efbf4fc9d8e254a
Romeo Rizzi <romeo.rizzi@univr.it>
2022-06-01 13:33:43 +0200

    added problem robot

---
+++ example_problems/tutorial/RO_robot/services/oracle_driver.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+from sys import exit
+
+from tabulate import tabulate
+
+from multilanguage import Env, Lang, TALcolors
+from TALfiles import TALfilesHelper
+
+import RO_problems_lib as RO
+import knapsack_lib
+
+# METADATA OF THIS TAL_SERVICE:
+args_list = [
+    ('pwd',str),
+    ('elementi','list_of_str'),
+    ('pesi','list_of_int'),
+    ('valori','list_of_int'),
+    ('Knapsack_Capacity',int),
+    ('elementi_proibiti','list_of_str'),
+    ('elementi_obbligati','list_of_str'),
+    ('sol_type',str),
+    ('name_of_opt_val',str),
+    ('name_of_opt_sol',str),
+    ('name_of_DPtable',str),
+    ('as_yaml',bool),
+    ('recall_input',bool),
+    ('with_opening_message',bool),
+    ('esercizio',int),
+    ('task',int),
+]
+
+ENV =Env(args_list)
+TAc =TALcolors(ENV, "None")
+LANG=Lang(ENV, TAc, lambda fstring: eval(f"f'{fstring}'"), print_opening_msg = 'now'if ENV['with_opening_message'] else 'never')
+TALf = TALfilesHelper(TAc, ENV)
+
+# START CODING YOUR SERVICE:
+
+TOKEN_REQUIRED = True
+RO.check_access_rights(ENV,TALf, require_pwd = True, TOKEN_REQUIRED = TOKEN_REQUIRED)
+knapsack_lib.check_request_consistency(ENV)
+                    
+opt_val, opt_sol, DPtable = knapsack_lib.solver(ENV)
+
+call_data = {'oracle': knapsack_lib.dict_of_oracle(ENV, opt_val,opt_sol,DPtable), 'input': knapsack_lib.dict_of_input(ENV) }
+
+RO.oracle_outputs(call_data,ENV)
+if ENV.LOG_FILES != None:
+    RO.oracle_logs(call_data,ENV,TALf)
+
