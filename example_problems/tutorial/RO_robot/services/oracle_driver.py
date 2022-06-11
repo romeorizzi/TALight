da22c91c78c3da725557febcceb5d40db808edf6
Romeo Rizzi <romeo.rizzi@univr.it>
2022-06-11 19:40:42 +0200

    instantiated problem RO_robot

--- example_problems/tutorial/RO_robot/services/oracle_driver.py
+++ example_problems/tutorial/RO_robot/services/oracle_driver.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
 
 from sys import exit, stderr
-
 from tabulate import tabulate
 
 from multilanguage import Env, Lang, TALcolors
 from TALfiles import TALfilesHelper
 
-import RO_problems_lib as RO
-from knapsack_lib import solver, check_instance_consistency
+import RO_std_io_lib as RO_io
+
+from robot_lib import solver, check_instance_consistency
 
 # METADATA OF THIS TAL_SERVICE:
 args_list = [
     ('pwd',str),
-    ('elementi','list_of_str'),
-    ('pesi','list_of_int'),
-    ('valori','list_of_int'),
+    ('labels','list_of_str'),
+    ('costs','list_of_int'),
+    ('vals','list_of_int'),
     ('Knapsack_Capacity',int),
-    ('elementi_proibiti','list_of_str'),
-    ('elementi_obbligati','list_of_str'),
+    ('forced_out','list_of_str'),
+    ('forced_in','list_of_str'),
     ('instance_dict','yaml'),
     ('request_dict','yaml'),
     ('as_yaml',bool),
@@ -29,7 +29,7 @@ args_list = [
     ('esercizio',int),
     ('task',int),
 ]
-instance_objects = ['elementi','pesi','valori','Knapsack_Capacity','elementi_proibiti','elementi_obbligati']
+instance_objects = ['labels','costs','vals','Knapsack_Capacity','forced_out','forced_in']
 sol_objects_implemented = ['opt_sol','opt_val','DPtable_opt_val']
 
 ENV =Env(args_list)
@@ -40,22 +40,22 @@ TALf = TALfilesHelper(TAc, ENV)
 # START CODING YOUR SERVICE:
 
 TOKEN_REQUIRED = True
-RO.check_access_rights(ENV,TALf, require_pwd = True, TOKEN_REQUIRED = TOKEN_REQUIRED)
-instance_dict = RO.dict_of_instance(instance_objects,args_list,ENV)
+RO_io.check_access_rights(ENV,TALf, require_pwd = True, TOKEN_REQUIRED = TOKEN_REQUIRED)
+instance_dict = RO_io.dict_of_instance(instance_objects,args_list,ENV)
 check_instance_consistency(instance_dict)
-RO.check_request(ENV['request_dict'], sol_objects_implemented)
+RO_io.check_request(ENV['request_dict'], sol_objects_implemented)
 
 request_dict = ENV["request_dict"]
 if len(request_dict) == 0:
     request_dict = { key:key for key in sol_objects_implemented }
-print(f"request_dict={request_dict}", file=stderr)
+#print(f"request_dict={request_dict}", file=stderr)
     
 call_data = {"instance":instance_dict,"request":request_dict}
 call_data["oracle"] = solver(call_data)
 
-RO.oracle_outputs(call_data,ENV)
+RO_io.oracle_outputs(call_data,ENV)
 if ENV.LOG_FILES != None:
-    RO.oracle_logs(call_data,ENV,TALf)
+    RO_io.oracle_logs(call_data,ENV,TALf)
 if ENV["with_output_files"]:    
-    RO.oracle_output_files(call_data,ENV,TALf)
+    RO_io.oracle_output_files(call_data,ENV,TALf)
 
