69942917ae3d3ffca01aa7c9de7a6618539cd099
unknown <giuliagalvan98@gmail.com>
2022-06-30 08:58:08 +0200

    limiti - fixed

--- example_problems/tutorial/limiti/services/density_Q_in_R_driver.py
+++ example_problems/tutorial/limiti/services/density_Q_in_R_driver.py
@@ -63,7 +63,7 @@ def check_q(x,y,n,user_int):
     if eval(q)!=correct_q:
         TAc.print(LANG.render_feedback("error", f'mmmm ci sei quasi! il numero razionale che hai inserito e\' compreso tra x ed y ma puoi fare di meglio: utilizza una relazione tra l\'intero che hai scelto (ovvero {user_int}) ed n={n} per creare un intero della forma \'a/b\', riprova:'), "yellow", ["bold"])
         return check_q(x,y,n,user_int)
-    return TAc.print(LANG.render_feedback("correct", f'Ben fatto! Abbiamo trovato la q che cercavamo, ovvero {q} = {eval(q)}. Abbiamo dimostrato che Q e\' denso in R!'), "green", ["bold"])
+    return TAc.print(LANG.render_feedback("correct", f'Ben fatto! Abbiamo trovato la q che cercavamo, infatti {q} = {eval(q)} e\' compresa tra x={x} ed y={y}.'), "green", ["bold"])
 
 def new_match(seed):
     TAc.print(LANG.render_feedback("seed", f'(seed: {seed})'), "yellow", ["bold"])
@@ -71,13 +71,13 @@ def new_match(seed):
     # print('y-x ',y-x)
     TAc.print(LANG.render_feedback("proposal", f'Le mie proposte sono:'), "yellow", ["bold"])
     TAc.print(LANG.render_feedback("proposal", f'x={x} \ny={y} '), "yellow", ["reverse"])
-    TAc.print(LANG.render_feedback("proposal", f'scrivi un numero naturale n > 0 che soddisfi il principio di Archimede (con argomento y-x):'), "yellow", ["bold"])
+    TAc.print(LANG.render_feedback("proposal", f'scrivi un numero naturale n > 0 che soddisfi il principio di Archimede (con argomento y-x): \n(se non ricordi questo principio ti consiglio l\'esercizio -> rtal connect limiti archimede_prover)'), "yellow", ["bold"])
     n=check_user_solution(y-x)
     nx=n*x
     ny=n*y
     TAc.print(LANG.render_feedback("next-step", f'Nota ora che {n}*x vale {nx} e {n}*y vale {ny} \ndimmi un intero in ({nx} , {ny}):'), "yellow", ["bold"])
     user_int=check_int_in_interval(nx,ny)
-    TAc.print(LANG.render_feedback("next-step", f'Utilizzando questo intero e la n che mi hai proposto, riusciresti a trovare un numero razionale (ovvero della forma a/b) compreso tra x={x} e y={y}? scrivilo:'), "yellow", ["bold"])
+    TAc.print(LANG.render_feedback("next-step", f'Utilizzando questo intero e la n che mi hai proposto prima, riusciresti a trovare un numero razionale (nella forma a/b) compreso tra x={x} e y={y}? scrivilo:'), "yellow", ["bold"])
     check_q(x,y,n,user_int)
 
 def what_to_do():
@@ -92,7 +92,7 @@ def what_to_do():
         what_to_do()
     else:
         assert answer_what_to_do=='livello_successivo'
-        TAc.print(LANG.render_feedback("algorithm", 'Ok! Ecco una proposta per il livello successivo: \nProva a scrivere (qui su terminale o immettendo un file) un algoritmo che dati x e y trovi q:'),  "yellow", ["bold"])
+        TAc.print(LANG.render_feedback("algorithm", 'Ok! Ecco una proposta per il livello successivo: \nProva a scrivere (qui su terminale o immettendo un file) un algoritmo che dati x e y trovi q seguendo la traccia di quello che abbiamo appena dimostrato insieme: se ci riuscirai avrai dimostrato la densita\' di Q in R! ;)'),  "yellow", ["bold"])
 
 def disprove():
     TAc.print(LANG.render_feedback("disprove", 'inserisci un qualsiasi valore reale x:'),  "yellow", ["bold"])
@@ -130,8 +130,7 @@ def no_case():
             else:
                 assert answer_what_to_do=='inverti_ruoli'
                 TAc.print(LANG.render_feedback("proof", 'Bene, sei pronto/a a dimostrarmi il principio di Archimede? Cominciamo subito!'), "white")
-                counter=0
-                new_match(ENV['seed'], counter)
+                new_match(ENV['seed'])
                 what_to_do()
         else:
             assert y_n=='n'
