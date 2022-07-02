1dc007642b0cf2737aaf6e156cd42ab4a26efd51
Giosuè <19361@studenti.marconiverona.edu.it>
2022-07-02 15:15:48 +0200

    miglioramento servizio first_ip

--- example_problems/tutorial/IPv4/services/first_ip_driver.py
+++ example_problems/tutorial/IPv4/services/first_ip_driver.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
+from ipaddress import ip_address
 from sys import stderr, exit
 
 import random
+from typing import Any
 from IPv4_lib import *
 from TALinputs import TALinput
 from multilanguage import Env, TALcolors, Lang
 args_list=[
-  ("subnet_Mask", str),
-  ("net_address", str),
+  ("ip_address", str),
 ]
 
 ENV =Env(args_list)
@@ -19,4 +20,14 @@ subnet=subnet_mask()
 internet=net_address(subnet)
 print("SUBNET MASK:  ",subnet)
 print("INTERNET ADDRESS:  ",internet)
-first_ip(internet)
+
+TAc.print("Inserire il primo indirizzo ip appartenente all'indirizo di rete qui sopra", "red", ["bold"])
+ip=(ENV[ip_address], TAc, LANG)
+separetedStrings= ip.split(".")
+numeriIP=[]
+for i in range (len(separetedStrings)) :
+  numeriIP.append( int(separetedStrings[i]))
+if internet[0] != numeriIP[0] or internet[1]!=numeriIP[1] or internet[2] != numeriIP[2] or internet[3]!=numeriIP[3] :
+  TAc.print("ERRORE\nRiprova", "red", ["bold"])
+else:
+  TAc.print("GIUSTO\n", "grenn", ["bold"])
