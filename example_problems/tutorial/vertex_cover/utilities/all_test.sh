c146e1a415504e9bb67d49614d7ae687bc23e2f9
Marco Oliani <marco.oliani@studenti.univr.it>
2022-09-11 16:44:32 +0200

    esempi_di_chiamate.txt updated

--- example_problems/tutorial/vertex_cover/utilities/all_test.sh
+++ example_problems/tutorial/vertex_cover/utilities/all_test.sh
@@ -7,21 +7,21 @@ make clean
 make
 
 ## gimme_instance
-rtal connect collage gimme_instance -asource=randgen_1 -anum_vertices=10 -anum_edges=15 -ainstance=simple
-rtal connect collage gimme_instance -asource=randgen_1 -anum_vertices=10 -anum_edges=15 -ainstance=with_info
-rtal connect collage gimme_instance -asource=randgen_1 -anum_vertices=10 -anum_edges=15 -aweighted=1 -ainstance=simple
-rtal connect collage gimme_instance -asource=randgen_1 -anum_vertices=10 -anum_edges=15 -aweighted=1 -ainstance=with_info
-rtal connect collage gimme_instance -asource=randgen_1 -anum_vertices=20 -anum_edges=30 -ainstance=simple
-rtal connect collage gimme_instance -asource=randgen_1 -anum_vertices=20 -anum_edges=30 -ainstance=with_info
-rtal connect collage gimme_instance -asource=randgen_1 -anum_vertices=20 -anum_edges=30 -aweighted=1 -ainstance=simple
-rtal connect collage gimme_instance -asource=randgen_1 -anum_vertices=20 -anum_edges=30 -aweighted=1 -ainstance=with_info
-rtal connect collage gimme_instance -asource=catalogue -ainstance_id=9 -ainstance_format=simple
-rtal connect collage gimme_instance -asource=catalogue -ainstance_id=9 -ainstance_format=with_len
-rtal connect collage gimme_instance -asource=catalogue -ainstance_id=15 -ainstance_format=simple
-rtal connect collage gimme_instance -asource=catalogue -ainstance_id=15 -ainstance_format=with_len
-rtal connect collage gimme_instance -asource=catalogue -ainstance_id=30 -ainstance_format=simple
-rtal connect collage gimme_instance -asource=catalogue -ainstance_id=42 -ainstance_format=with_len
-rtal connect collage gimme_instance -asource=catalogue -ainstance_id=42 -ainstance_format=simple
+rtal connect vertex_cover gimme_instance -asource=randgen_1 -anum_vertices=10 -anum_edges=15 -ainstance=simple
+rtal connect vertex_cover gimme_instance -asource=randgen_1 -anum_vertices=10 -anum_edges=15 -ainstance=with_info
+rtal connect vertex_cover gimme_instance -asource=randgen_1 -anum_vertices=10 -anum_edges=15 -aweighted=1 -ainstance=simple
+rtal connect vertex_cover gimme_instance -asource=randgen_1 -anum_vertices=10 -anum_edges=15 -aweighted=1 -ainstance=with_info
+rtal connect vertex_cover gimme_instance -asource=randgen_1 -anum_vertices=20 -anum_edges=30 -ainstance=simple
+rtal connect vertex_cover gimme_instance -asource=randgen_1 -anum_vertices=20 -anum_edges=30 -ainstance=with_info
+rtal connect vertex_cover gimme_instance -asource=randgen_1 -anum_vertices=20 -anum_edges=30 -aweighted=1 -ainstance=simple
+rtal connect vertex_cover gimme_instance -asource=randgen_1 -anum_vertices=20 -anum_edges=30 -aweighted=1 -ainstance=with_info
+rtal connect vertex_cover gimme_instance -asource=catalogue -ainstance_id=9 -ainstance_format=simple
+rtal connect vertex_cover gimme_instance -asource=catalogue -ainstance_id=9 -ainstance_format=with_len
+rtal connect vertex_cover gimme_instance -asource=catalogue -ainstance_id=15 -ainstance_format=simple
+rtal connect vertex_cover gimme_instance -asource=catalogue -ainstance_id=15 -ainstance_format=with_len
+rtal connect vertex_cover gimme_instance -asource=catalogue -ainstance_id=30 -ainstance_format=simple
+rtal connect vertex_cover gimme_instance -asource=catalogue -ainstance_id=42 -ainstance_format=with_len
+rtal connect vertex_cover gimme_instance -asource=catalogue -ainstance_id=42 -ainstance_format=simple
 
 ## chech_minimum_vc
 rtal connect vertex_cover check_minimum_vc -asource=randgen_1
