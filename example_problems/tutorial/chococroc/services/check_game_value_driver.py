5738a770ae79d4e242545d72b0081badec6bb265
Romeo Rizzi <romeo.rizzi@univr.it>
2022-09-12 12:28:28 +0200

    vice check_value for some games

--- example_problems/tutorial/chococroc/services/check_game_value_driver.py
+++ example_problems/tutorial/chococroc/services/check_game_value_driver.py
@@ -17,43 +17,37 @@ args_list = [
 
 ENV =Env(args_list)
 TAc =TALcolors(ENV)
-LANG=Lang(ENV, TAc, lambda fstring: eval(f"f'{fstring}'"))
+LANG=Lang(ENV, TAc, lambda fstring: eval(f"f'{fstring}'"), print_opening_msg = 'delayed' if ENV['silent'] else 'now')
 
 # START CODING YOUR SERVICE:
 grundy_val = cl.grundy_val(ENV['m'], ENV['n'])
 #print(f"grundy_val={grundy_val}")
-if ENV['value'] == -2:
+if ENV['value'] == -2: # user conjectures this is a first_to_move_wins position
     if grundy_val == 0:
-        TAc.NO()
-        TAc.print(LANG.render_feedback("not-a-winning-conf", f'Contrary to your conjecture, the configuration {ENV["m"]} x {ENV["n"]} is NOT a winning one.'), "red")
+        TAc.print(LANG.render_feedback("not-a-winning-conf", f'No! Contrary to your conjecture, the configuration {ENV["m"]} x {ENV["n"]} is NOT a winning one.'), "red")
         TAc.print(LANG.render_feedback("not-a-winning-conf-wanna-play", f'You can check this out playing a game against our service \'play\', starting first on configuration {ENV["m"]} x {ENV["n"]}. If you succeed winning then you disprove our claim or the optimality of our player (either way, let us know).'), "yellow", ["bold"])
     elif not ENV['silent']:
-        TAc.OK()
-        TAc.print(LANG.render_feedback("ok-winning-conf", f'We agree with your conjecture that the configuration {ENV["m"]} x {ENV["n"]} is a winning one.'), "green", ["bold"])
+        TAc.print(LANG.render_feedback("ok-winning-conf", f'Ok! We agree with your conjecture that the configuration {ENV["m"]} x {ENV["n"]} is a winning one.'), "green", ["bold"])
 
-if ENV['value'] == -1:
+if ENV['value'] == -1: # user conjectures this is a first_to_move_loses position
     if grundy_val != 0:
-        TAc.NO()
-        TAc.print(LANG.render_feedback("not-a-lost-conf", f'Contrary to your conjecture, the configuration {ENV["m"]} x {ENV["n"]} is NOT a lost one.'), "red")
+        TAc.print(LANG.render_feedback("not-a-lost-conf", f'No! Contrary to your conjecture, the configuration {ENV["m"]} x {ENV["n"]} is NOT a lost one.'), "red")
         TAc.print(LANG.render_feedback("not-a-lost-conf-wanna-play", f'You can check this out playing a game against our service \'play\', playing as second a game starting from configuration {ENV["m"]} x {ENV["n"]}. If you succeed winning then you disprove our claim or the optimality of our player (either way, let us know).'), "yellow", ["bold"])
     elif not ENV['silent']:
-        TAc.OK()
-        TAc.print(LANG.render_feedback("ok-lost-conf", f'We agree with your conjecture that the configuration {ENV["m"]} x {ENV["n"]} is a lost one.'), "green", ["bold"])
+        TAc.print(LANG.render_feedback("ok-lost-conf", f'Ok! We agree with your conjecture that the configuration {ENV["m"]} x {ENV["n"]} is a lost one.'), "green", ["bold"])
 
-if ENV['value'] >= 0:
+if ENV['value'] >= 0:  # user conjectures that the Grundy value of the position ENV["number"] is ENV['value']
     if grundy_val != ENV['value']:
-        TAc.NO()
-        TAc.print(LANG.render_feedback("wrong-grundy-val", f'Contrary to your conjecture, the grundy value of configuration {ENV["m"]} x {ENV["n"]} is NOT {ENV["value"]}.'), "red")
+        TAc.print(LANG.render_feedback("wrong-grundy-val", f'No! Contrary to your conjecture, the grundy value of configuration {ENV["m"]} x {ENV["n"]} is NOT {ENV["value"]}.'), "red")
         if grundy_val * ENV['value'] != 0:
             TAc.print(LANG.render_feedback("wrong-grundy-val-play", f'You can check this out playing a game against our service \'play_val_measuring_game\', starting second on configuration (chocolate_bar={ENV["m"]} x {ENV["n"]}, single_NIM_tower={ENV["value"]}). If you succeed winning then you disprove our claim or the optimality of our player (either way, let us know).'), "yellow", ["bold"])
         elif grundy_val == 0:
-            TAc.print(LANG.render_feedback("not-a-winning-conf", f'Contrary to your conjecture, the configuration {ENV["m"]} x {ENV["n"]} is NOT a winning one.'), "red")
+            TAc.print(LANG.render_feedback("not-a-winning-conf", f'Indeed, the configuration {ENV["m"]} x {ENV["n"]} is NOT a winning one for the first player to move.'), "red")
             TAc.print(LANG.render_feedback("not-a-winning-conf-wanna-play", f'You can check this out playing a game against our service \'play\', starting first on configuration {ENV["m"]} x {ENV["n"]}. If you succeed winning then you disprove our claim or the optimality of our player (either way, let us know).'), "yellow", ["bold"])
         else:    
-            TAc.print(LANG.render_feedback("not-a-lost-conf", f'Contrary to your conjecture, the configuration {ENV["m"]} x {ENV["n"]} is NOT a lost one.'), "red")
+            TAc.print(LANG.render_feedback("not-a-lost-conf", f'Indeed, the configuration {ENV["m"]} x {ENV["n"]} is NOT a lost one for the first player to move.'), "red")
             TAc.print(LANG.render_feedback("not-a-lost-conf-wanna-play", f'You can check this out playing a game against our service \'play\', playing as second a game starting from configuration {ENV["m"]} x {ENV["n"]}. If you succeed winning then you disprove our claim or the optimality of our player (either way, let us know).'), "yellow", ["bold"])
     elif not ENV['silent']:
-        TAc.OK()
-        TAc.print(LANG.render_feedback("ok-grundy-val", f'We agree with your conjecture that the configuration {ENV["m"]} x {ENV["n"]} has grundy value {grundy_val}.'), "green", ["bold"])
+        TAc.print(LANG.render_feedback("ok-grundy-val", f'Ok! We agree with your conjecture that the configuration {ENV["m"]} x {ENV["n"]} has grundy value {grundy_val}.'), "green", ["bold"])
 
 exit(0)
--- example_problems/tutorial/game_path_in_graph/services/check_game_value_driver.py
+++ example_problems/tutorial/game_path_in_graph/services/check_game_value_driver.py
@@ -18,7 +18,7 @@ args_list = [
 
 ENV =Env(args_list)
 TAc =TALcolors(ENV)
-LANG=Lang(ENV, TAc, lambda fstring: eval(f"f'{fstring}'"))
+LANG=Lang(ENV, TAc, lambda fstring: eval(f"f'{fstring}'"), print_opening_msg = 'delayed' if ENV['silent'] else 'now')
 
 # START CODING YOUR SERVICE:
 graph=gl.generate_graph_from_input(ENV['graph'])
@@ -28,22 +28,18 @@ if not(ENV['starting_node'] in gl.get_nodes(graph)):
 f_player_w = gl.first_player_win(graph)
 moves=gl.find_winning_moves_on_graph_not_winning(graph,ENV['starting_node'],True)
 #print(f"grundy_val={grundy_val}")
-if ENV['value'] == -2:
+if ENV['value'] == -2: # user conjectures this is a first_to_move_wins position
     if not(f_player_w) and moves==[]:
-        TAc.NO()
-        TAc.print(LANG.render_feedback("not-a-winning-graph", f'Contrary to your conjecture, the graph \'{ENV["graph"]}\' is NOT a winning one.'), "red")
+        TAc.print(LANG.render_feedback("not-a-winning-graph", f'No! Contrary to your conjecture, the graph \'{ENV["graph"]}\' is NOT a winning one.'), "red")
         TAc.print(LANG.render_feedback("not-a-winning-graph-wanna-play", f'You can check this out playing a game against our service \'play\', starting first on graph \'{ENV["graph"]}\' from node {ENV["starting_node"]}. If you succeed winning then you disprove our claim or the optimality of our player (either way, let us know).'), "yellow", ["bold"])
     elif not ENV['silent']:
-        TAc.OK()
-        TAc.print(LANG.render_feedback("ok-winning-graph", f'We agree with your conjecture that the graph \'{ENV["graph"]}\' is a winning one.'), "green", ["bold"])
+        TAc.print(LANG.render_feedback("ok-winning-graph", f'Ok! We agree with your conjecture that the graph \'{ENV["graph"]}\' is a winning one.'), "green", ["bold"])
 
-if ENV['value'] == -1:
+if ENV['value'] == -1: # user conjectures this is a first_to_move_loses position
     if f_player_w or moves!=[]:
-        TAc.NO()
-        TAc.print(LANG.render_feedback("not-a-lost-graph", f'Contrary to your conjecture, the graph \'{ENV["graph"]}\' is NOT a lost one.'), "red")
+        TAc.print(LANG.render_feedback("not-a-lost-graph", f'No! Contrary to your conjecture, the graph \'{ENV["graph"]}\' is NOT a lost one.'), "red")
         TAc.print(LANG.render_feedback("not-a-lost-graph-wanna-play", f'You can check this out playing a game against our service \'play\', playing as second a game starting from graph \'{ENV["graph"]}\' from node {ENV["starting_node"]}. If you succeed winning then you disprove our claim or the optimality of our player (either way, let us know).'), "yellow", ["bold"])
     elif not ENV['silent']:
-        TAc.OK()
-        TAc.print(LANG.render_feedback("ok-lost-graph", f'We agree with your conjecture that the graph \'{ENV["graph"]}\' is a lost one.'), "green", ["bold"])
+        TAc.print(LANG.render_feedback("ok-lost-graph", f'Ok! We agree with your conjecture that the graph \'{ENV["graph"]}\' is a lost one.'), "green", ["bold"])
 
 exit(0)
