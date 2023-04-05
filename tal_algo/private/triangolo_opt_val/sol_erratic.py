e3df4a2f86035f173a5862820ab03ad8baa8e6f2
Romeo Rizzi <romeo.rizzi@univr.it>
2023-04-05 08:16:41 +0200

    added triangolo_opt_val

--- tal_algo/private/triangolo_opt_val/sol_erratic.py
+++ tal_algo/private/triangolo_opt_val/sol_erratic.py
@@ -34,6 +34,6 @@ if __name__ == "__main__":
         #display_triangle(Tr)
         dice = randrange(0,6)
         if dice == 0:
-            print(max_val(Tr)+1)
+            print(max_val(Tr)-1)
         else:
             print(max_val(Tr))
