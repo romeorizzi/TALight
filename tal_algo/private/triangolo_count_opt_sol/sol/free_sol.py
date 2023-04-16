f444e8e63b9e198fd5f19410b6c5c9ae17833afc
Romeo Rizzi <romeo.rizzi@univr.it>
2023-04-16 12:32:06 +0200

    added free_sol to triangolo problems

---
+++ tal_algo/private/triangolo_count_opt_sol/sol/free_sol.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from sys import stderr
+
+from triangolo_lib import max_val, num_opt_sols, display_triangle
+
+if __name__ == "__main__":
+    T = int(input())
+    for t in range(T):
+        n = int(input())
+        Tr = []
+        for i in range(n):
+            Tr.append(list(map(int,input().strip().split())))
+        #display_triangle(Tr,stderr)
+        #print(max_val(Tr), file=stderr)
+        #print(num_opt_sols(Tr), file=stderr)
+        print(max_val(Tr))
+        print(num_opt_sols(Tr))
