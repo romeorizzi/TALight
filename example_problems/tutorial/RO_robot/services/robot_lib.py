Fixed bug in computation of the optimal path gain

example_problems/tutorial/RO_robot/services/robot_lib.py
@@ -3,7 +3,6 @@
 from dataclasses import dataclass
 from typing import Dict, Final, List, Tuple, TypeVar
 import itertools
-import re
 
 import numpy as np
 
@@ -578,7 +577,7 @@ def query_num(num_beg2any: np.ndarray, num_any2end: np.ndarray, through: _Cell):
     return sum(solutions)
 
 
-def query_opt(opt_beg2any: np.ndarray, opt_any2end: np.ndarray, through: _Cell):
+def query_opt(grid: np.ndarray, opt_beg2any: np.ndarray, opt_any2end: np.ndarray, through: _Cell):
     assert opt_beg2any.shape == opt_any2end.shape
     x, y = through
     budget = opt_beg2any.shape[0]  # dptable matrix is [budget][row][col]
@@ -590,7 +589,9 @@ def query_opt(opt_beg2any: np.ndarray, opt_any2end: np.ndarray, through: _Cell):
         solutions.append(opt_A_to_B + opt_B_to_C)
 
     # pick the best solution
-    return max(solutions)
+    # NOTE: all solutions include the gain of the <through> cell two times,
+    # we need to remove it once
+    return max(solutions) - cell_gain(grid[x][y])
 
 
 def query_num_opt(
@@ -662,7 +663,7 @@ def solver(input_to_oracle):
     assert DPtable_num_opt_from.shape == expected_dptable_shape
 
     num_paths = query_num(DPtable_num_to, DPtable_num_from, mid)
-    opt_val = query_opt(DPtable_opt_to, DPtable_opt_from, mid)
+    opt_val = query_opt(grid, DPtable_opt_to, DPtable_opt_from, mid)
     num_opt_paths = query_num_opt(DPtable_opt_to, DPtable_opt_from,
                                   DPtable_num_opt_to, DPtable_num_opt_from, mid)
 

example_problems/tutorial/RO_robot/services/test_unit.py
@@ -4,7 +4,7 @@ import numpy.testing as npt
 from robot_lib import *
 
 
-class InputParsing(unittest.TestCase):
+class InputParsingTest(unittest.TestCase):
     def test_raw_cell_parsing(self):
         testcases = [
             (["10", "a"], (9, 0)),
@@ -51,7 +51,7 @@ class UilityFunctions(unittest.TestCase):
                 self.assertEqual(expected_cost, cell_cost(content))
 
 
-class DPTableGenerators(unittest.TestCase):
+class DPTableGeneratorsTest(unittest.TestCase):
 
     def test_num_to_cell(self):
         grid = [[0, -1, 2],
@@ -174,7 +174,18 @@ class DPTableGenerators(unittest.TestCase):
         npt.assert_array_equal(actual, expect)
 
 
-class PathGenerators(unittest.TestCase):
+class DPTableQueryTest(unittest.TestCase):
+    
+    def test_query_opt(self):
+        grid = [[0, 1, 0, 3, 0], [-1, 2,-1,-1, 0], [0, 0, 2, 1, 0], [0,-1, 0, 1, 1] ]
+        grid = np.array(grid)
+        beg2any = dp_opt_to(grid, (0,0), budget=0, diag=False)
+        any2end = dp_opt_from(grid, (3,4), budget=0, diag=False)
+        opt = query_opt(grid, opt_beg2any=beg2any, opt_any2end=any2end, through=(2,2))
+        self.assertEqual(opt, 8)
+
+
+class PathGeneratorsTest(unittest.TestCase):
 
     def test_yield_opt_paths_beg2mid(self):
         testcases = [